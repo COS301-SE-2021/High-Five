26c4a4ea2c21b149d43b25dbc5e4943c51674e00
Jeanre <jeanreduplessis2000@gmail.com>
2021-05-28 23:52:01 +0200

    Basic app flow (pre login) completed

--- mobile/src/app/pages/login/login-routing.module.ts
+++ mobile/src/app/pages/login/login-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginPage } from './login.page';
+import {WelcomePage} from "../welcome/welcome.page";
 
 const routes: Routes = [
   {
     path: '',
     component: LoginPage
+  },
+  {
+    path: 'welcome',
+    component: WelcomePage
   }
 ];
 
