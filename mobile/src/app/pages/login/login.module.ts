0710efb7d4026fcf9e8358a3615be54ff23a9c7f
Jeanre <jeanreduplessis2000@gmail.com>
2021-05-28 22:51:21 +0200

    Switched to using components to create login page's card

--- mobile/src/app/pages/login/login.module.ts
+++ mobile/src/app/pages/login/login.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { LoginPageRoutingModule } from './login-routing.module';
+import {LoginPageRoutingModule} from './login-routing.module';
 
-import { LoginPage } from './login.page';
+import {LoginPage} from './login.page';
+import {LoginCardComponent} from '../../components/login-card/login-card.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { LoginPage } from './login.page';
     IonicModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage, LoginCardComponent]
 })
-export class LoginPageModule {}
+export class LoginPageModule {
+}
