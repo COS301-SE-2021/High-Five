b772511c4f09a8231f37fb161bc4179ef639c071
Jeanre <jeanreduplessis2000@gmail.com>
2021-05-28 18:29:38 +0200

    updated app name from "mobile" to "highfive"

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'mobile',
+  appName: 'highfive',
   webDir: 'www',
   bundledWebRuntime: false
 };
