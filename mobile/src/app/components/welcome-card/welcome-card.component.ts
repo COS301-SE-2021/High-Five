a0cc15bebdc7f39b0a4b90eb83332a15aee98ce5
Jeanré du Plessis <47949489+JayXD-2K@users.noreply.github.com>
2021-05-28 23:53:51 +0200

    Basic app flow (pre login) completed (#9)

---
+++ mobile/src/app/components/welcome-card/welcome-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-card',
+  templateUrl: './welcome-card.component.html',
+  styleUrls: ['./welcome-card.component.scss'],
+})
+export class WelcomeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
