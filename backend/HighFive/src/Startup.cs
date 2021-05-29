7682e7eebeb2f0a1dc508fbc183031ddabc11e73
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-05-29 16:04:44 +0200

    Changed private attributes back to public, because of C# accessor/mutator functionality

--- backend/HighFive/src/Startup.cs
+++ backend/HighFive/src/Startup.cs
@@ -23,7 +23,7 @@ namespace src
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration;
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
