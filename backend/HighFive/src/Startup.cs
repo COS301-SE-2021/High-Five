18c68232be55d7dbf649fb9f43677a21edac2707
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-05-29 14:58:54 +0200

    Succesfully connected EF Core, User Model to a local SQLExpress DB.

--- backend/HighFive/src/Startup.cs
+++ backend/HighFive/src/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using src.Resources;
 
 namespace src
 {
@@ -21,13 +23,18 @@ namespace src
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "src", Version = "v1"}); });
+
+            services.AddDbContext<HighFiveContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("MyConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
