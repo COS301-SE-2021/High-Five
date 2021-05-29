822747d831bb8062ddd4f6055ab20663ea2de485
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-05-29 16:04:44 +0200

    Added UserQueries class as an example, still to be discussed with the team

--- backend/HighFive/src/Subsystems/User/Data/User.cs
+++ backend/HighFive/src/Subsystems/User/Data/User.cs
@@ -7,22 +7,22 @@ namespace src.Subsystems.User.Data
     {
         #nullable enable
         [Key] 
-        private Guid UserId { get; set; }
+        public Guid UserId { get; set; }
 
-        private string FirstName { get; set; }
+        public string FirstName { get; set; }
 
-        private string LastName { get; set; }
+        public string LastName { get; set; }
 
-        private string Username { get; set; }
+        public string Username { get; set; }
 
-        private string Password { get; set; }
+        public string Password { get; set; }
 
         [Required]
-        private string Email { get; set; }
+        public string Email { get; set; }
         
-        private string? ActivationCode { get; set; }
+        public string? ActivationCode { get; set; }
         
-        private string? PasswordResetCode { get; set; }
+        public string? PasswordResetCode { get; set; }
         
     }
 }
