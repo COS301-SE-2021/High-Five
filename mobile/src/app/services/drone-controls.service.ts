3f4c16ae9969edf9c59520b3ec108674da98773c
Hannes <hanneskirsten00@gmail.com>
2021-05-31 14:58:03 +0200

    Drone control service added

--- mobile/src/app/services/drone-controls.service.ts
+++ mobile/src/app/services/drone-controls.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {Drone} from 'dji-tello';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DroneControlsService {
 
+
+
   constructor() { }
+
 }
