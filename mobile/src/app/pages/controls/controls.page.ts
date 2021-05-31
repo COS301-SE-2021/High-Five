88b757850ba52bb82e0f4177e236c93dd1a94a62
Hannes <hanneskirsten00@gmail.com>
2021-05-31 16:09:45 +0200

    Drone control page functionality added

--- mobile/src/app/pages/controls/controls.page.ts
+++ mobile/src/app/pages/controls/controls.page.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {DroneControlsService} from '../../services/drone-controls.service';
 @Component({
   selector: 'app-controls',
   templateUrl: './controls.page.html',
   styleUrls: ['./controls.page.scss'],
 })
 export class ControlsPage implements OnInit {
-
-  constructor() {  }
+  private drone;
+  constructor(private droneControlsService: DroneControlsService) {
+    this.drone=droneControlsService;
+  }
 
   ngOnInit() {
+  }
+
+  demo(){
+    this.drone.forward(100);
+    this.drone.left(100);
+  }
+
+  takeoff(){
+    this.drone.takeoff();
+  }
+
+  land(){
+    this.drone.land();
+  }
+
+  connect(){
+    this.drone.connect();
   }
 
 }
