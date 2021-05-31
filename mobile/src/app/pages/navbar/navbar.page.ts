c15daeeb670a95e62044d49e3a071077e978ee38
darthcerellius <71949550+darthcerellius@users.noreply.github.com>
2021-05-31 12:53:32 +0200

    Mobile feature landing (#26)
    * Added landing page and desktop navbar as well as controls page
    
    * Added : Routing for navbars, analytics page, controls page,
    
    WIP : Fix navbar/navbar routing & can't route to different page after initial routing on desktop navbar click
    
    * Added screen @ionic-native/screen-orientation as a dependency
    
    To be used by the controls page later in the project
    
    * Corrected routing link in login-card.component.html
    
    * Fixed double navbar url issue
    
    * Trying to fix navbar. - Kyle Barry (u19232510)
    
    * Managed to fix the navigation system. - Kyle Barry (u19232510)
    
    Co-authored-by: Jeanré <jeanreduplessis2000@gmail.com>
    Co-authored-by: Jeanré du Plessis <47949489+JayXD-2K@users.noreply.github.com>

--- mobile/src/app/pages/navbar/navbar.page.ts
+++ mobile/src/app/pages/navbar/navbar.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ScreenSizeServiceService} from '../../services/screen-size-service.service';
+import {IonButton} from "@ionic/angular";
+import {Navigation, Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -8,13 +10,39 @@ import {ScreenSizeServiceService} from '../../services/screen-size-service.servi
 })
 export class NavbarPage implements OnInit {
 
+  @ViewChild('homeNav') homeNav : HTMLIonButtonElement;
+  @ViewChild('analyticsNav') analyticsNav : HTMLIonButtonElement;
+  @ViewChild('videoNav') videoNav : HTMLIonButtonElement;
+
+  private navPages;
+  homeLink = ["active-link"];
+  analyticsLink = ["link"];
+  videoLink = ["link"];
+
   isDesktop: boolean;
-  constructor(private screenSizeService: ScreenSizeServiceService) {
+  constructor(private screenSizeService: ScreenSizeServiceService, private nav : Router) {
     this.screenSizeService.isDesktopView().subscribe(isDesktop=>{
       this.isDesktop = isDesktop;
     });
+    this.navPages = {
+      'homeNav' : this.homeLink,
+      'analyticsNav' : this.analyticsLink,
+      'videoNav' : this.videoLink
+    }
   }
   ngOnInit() {
+  }
+
+  navigateTo(url : String, tab : String) {
+    for (let key in this.navPages) {
+      let value = this.navPages[key];
+      if (key !== tab) {
+        value[0] = 'link';
+      } else {
+        value[0] = 'active-link';
+      }
+    }
+    this.nav.navigate([url]);
   }
 
 }
