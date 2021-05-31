f5a90f0e24b7d8caeee17c05cf8d9a5ef7e294ad
Kyle Barry <u19232510@tuks.co.za>
2021-05-31 12:02:30 +0200

    Trying to fix navbar. - Kyle Barry (u19232510)

--- mobile/src/app/pages/navbar/navbar.page.ts
+++ mobile/src/app/pages/navbar/navbar.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ScreenSizeServiceService} from '../../services/screen-size-service.service';
+import {IonButton} from "@ionic/angular";
+import {Navigation, Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -8,13 +10,43 @@ import {ScreenSizeServiceService} from '../../services/screen-size-service.servi
 })
 export class NavbarPage implements OnInit {
 
+  @ViewChild('homeNav') homeNav : HTMLIonButtonElement;
+  @ViewChild('analyticsNav') analyticsNav : HTMLIonButtonElement;
+  @ViewChild('videoNav') videoNav : HTMLIonButtonElement;
+
+  private navPages;
+  homeLink = "active-link";
+  analyticsLink = "link";
+  videoLink = "link";
+
   isDesktop: boolean;
-  constructor(private screenSizeService: ScreenSizeServiceService) {
+  constructor(private screenSizeService: ScreenSizeServiceService, private nav : Router) {
     this.screenSizeService.isDesktopView().subscribe(isDesktop=>{
       this.isDesktop = isDesktop;
     });
   }
   ngOnInit() {
+    this.navPages = {
+      'homeNav' : this.homeNav,
+      'analyticsNav' : this.analyticsNav,
+      'videoNav' : this.videoNav
+    }
+    console.log(this.homeNav)
+  }
+
+  navigateTo(url : String, tab : String) {
+    this.nav.navigate([url]);
+
+
+    for (let key in this.navPages) {
+      console.log(this.navPages[key])
+      let value = this.navPages[key];
+      if (key !== tab) {
+        value.setAttribute('class', 'link');
+      } else {
+        value.setAttribute('class', 'active-link');
+      }
+    }
   }
 
 }
