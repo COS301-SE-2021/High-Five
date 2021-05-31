c0b7c90e79e914dc7dde68e8e9f8888d47d897cc
Hannes <hanneskirsten00@gmail.com>
2021-05-31 16:03:13 +0200

    Drone control functions added

--- mobile/src/app/services/drone-controls.service.ts
+++ mobile/src/app/services/drone-controls.service.ts
@@ -6,8 +6,52 @@ import {Drone} from 'dji-tello';
 })
 export class DroneControlsService {
 
+  constructor() {
+    Drone.connect();
+  }
 
+  takeoff(){
+    Drone.takeoff();
+  }
 
-  constructor() { }
+  land(){
+    Drone.land();
+  }
+
+  stop(){
+    Drone.event('stop');
+  }
+
+  forward(x){
+    Drone.forward(x);
+  }
+
+  backward(x){
+    Drone.back(x);
+  }
+
+  left(x){
+    Drone.left(x);
+  }
+
+  right(x){
+    Drone.right(x);
+  }
+
+  up(x){
+    Drone.event('up '+x);
+  }
+
+  down(x){
+    Drone.event('down '+x);
+  }
+
+  rotateLeft(x){
+    Drone.event('ccw '+x);
+  }
+
+  rotateRight(x){
+    Drone.event('cw '+x);
+  }
 
 }
