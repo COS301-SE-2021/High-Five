4c143a840f43bc9f296c3fab319528e89b10580e
Kyle Barry <u19232510@tuks.co.za>
2021-05-31 14:44:50 +0200

    Started work on getting video playback working - Kyle Barry (u19232510)

---
+++ mobile/src/app/components/videostream-card/videostream-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videostream-card',
+  templateUrl: './videostream-card.component.html',
+  styleUrls: ['./videostream-card.component.scss'],
+})
+export class VideostreamCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
