c715ead466a2477191666a4e68edd267bdeec6af
Jeanre <jeanreduplessis2000@gmail.com>
2021-05-31 13:12:16 +0200

    Added video streaming plugin and dependecy

--- mobile/src/app/pages/controls/controls.page.ts
+++ mobile/src/app/pages/controls/controls.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {StreamingMedia, StreamingVideoOptions} from "@ionic-native/streaming-media/ngx";
 
 @Component({
   selector: 'app-controls',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./controls.page.scss'],
 })
 export class ControlsPage implements OnInit {
-  constructor() {}
+
+  constructor(private streamingMedia: StreamingMedia) {  }
+
+
+  startVideo(){
+    let options: StreamingVideoOptions = {
+      successCallback: () => { console.log('Video played') },
+      errorCallback: (e) => { console.log('Error streaming') },
+      orientation: 'portrait',
+    };
+    this.streamingMedia.playVideo('/media/jeanre/Additional/University/2021/Semester 1/COS 314/Lectures/L6.mp4',options);
+  }
 
   ngOnInit() {
   }
