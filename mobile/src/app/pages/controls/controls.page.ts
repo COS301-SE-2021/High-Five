d432afba7adde0ab20d72a578cd7d16436391243
Jeanre <jeanreduplessis2000@gmail.com>
2021-06-01 08:53:22 +0200

    Removed UDP Plugins

--- mobile/src/app/pages/controls/controls.page.ts
+++ mobile/src/app/pages/controls/controls.page.ts
@@ -1,34 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import {DroneControlsService} from '../../services/drone-controls.service';
+
 @Component({
   selector: 'app-controls',
   templateUrl: './controls.page.html',
   styleUrls: ['./controls.page.scss'],
 })
 export class ControlsPage implements OnInit {
-  private drone;
-  constructor(private droneControlsService: DroneControlsService) {
-    this.drone=droneControlsService;
+  constructor() {
+
   }
 
   ngOnInit() {
+
   }
 
   demo(){
-    this.drone.forward(100);
-    this.drone.left(100);
+
   }
 
   takeoff(){
-    this.drone.takeoff();
+
   }
 
   land(){
-    this.drone.land();
+
   }
 
-  connect(){
-    this.drone.connect();
+  connect() {
   }
 
 }
