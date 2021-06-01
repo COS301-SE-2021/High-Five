acf360591a0ef0c78e39a2f040b0cd55c62a3121
Kyle Barry <u19232510@tuks.co.za>
2021-06-01 09:32:51 +0200

    Tidied up code and added documentation - Kyle Barry (u19232510)

--- mobile/src/app/components/videostore-card/videostore-card.component.ts
+++ mobile/src/app/components/videostore-card/videostore-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {VideoPreviewData} from "../../pages/videostore/videostore.page";
 import {ModalController, Platform} from "@ionic/angular";
 import {VideostreamCardComponent} from "../videostream-card/videostream-card.component";
@@ -10,54 +10,23 @@ import {VideostreamCardComponent} from "../videostream-card/videostream-card.com
 })
 export class VideostoreCardComponent implements OnInit {
   @Input() data: VideoPreviewData;  //be specific later
-  @ViewChild('desktopImage') desktopImage : HTMLImageElement;
-  @ViewChild('mobileImage') mobileImage : HTMLImageElement;
-
-  private mobile : boolean;
 
   constructor(public platform: Platform, private modal: ModalController) { }
 
   ngOnInit() {
   }
 
-  private isMobile() : boolean {
-    return this.platform.width() < 700;
-  }
-
-  onResize(event) {
-    if (this.isMobile()) {
-      if (this.desktopImage != undefined && this.mobileImage != undefined) {
-        this.desktopImage.style.display = "none";
-        this.mobileImage.style.display = "block";
-      }
-    } else {
-      if (this.desktopImage != undefined && this.mobileImage != undefined) {
-        this.desktopImage.style.display = "block";
-        this.mobileImage.style.display = "none";
-      }
-    }
-  }
-  onLoad(event) {
-    if (this.isMobile()) {
-      if (this.desktopImage != undefined && this.mobileImage != undefined) {
-        this.desktopImage.style.display = "none";
-        this.mobileImage.style.display = "block";
-      }
-    } else {
-      if (this.desktopImage != undefined && this.mobileImage != undefined) {
-        this.desktopImage.style.display = "block";
-        this.mobileImage.style.display = "none";
-      }
-    }
-  }
-
+  /**
+   * This function creates a modal where the recorded drone footage can be
+   * replayed to the user.
+   */
   async playVideo() {
     console.log("playing video")
     const videoModal = await this.modal.create({
       component: VideostreamCardComponent
     })
 
-    videoModal.style.backgroundColor = "rgba(0,0,0,0.85)"
+    videoModal.style.backgroundColor = "rgba(0,0,0,0.85)" //make the background for the modal darker.
 
     await videoModal.present();
   }
--- mobile/src/app/components/videostream-card/videostream-card.component.ts
+++ mobile/src/app/components/videostream-card/videostream-card.component.ts
@@ -6,25 +6,9 @@ import {Component, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./videostream-card.component.scss'],
 })
 export class VideostreamCardComponent implements OnInit {
-  @ViewChild('playMedia') playMedia : HTMLVideoElement;
-  @ViewChild('videoPlay') vidPlay : HTMLIonButtonElement;
-  @ViewChild('videoPause') vidPause : HTMLIonButtonElement;
 
   constructor() { }
 
   ngOnInit() {}
-
-  playVideo() {
-    console.log(this.vidPlay)
-    this.vidPlay['el'].style.display = "none";
-    this.vidPause['el'].style.display = "block";
-    this.playMedia['nativeElement'].play(); // WHY do I need to access nativeElement before playing?
-  }
-
-  pauseVideo() {
-    this.vidPlay['el'].style.display = "block";
-    this.vidPause['el'].style.display = "none";
-    this.playMedia['nativeElement'].pause(); // WHY do I need to access nativeElement before playing?
-  }
 
 }
