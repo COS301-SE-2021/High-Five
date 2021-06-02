c8cf6728190b59305a9fcf769ed4aeac408ced39
ruan-bieldt <u13145992@tuks.co.za>
2021-06-02 12:57:20 +0200

    Added calls to start and stop videoServer

--- plugins/capacitor-udp-requests/android/src/main/java/com/bdpsolutions/plugin/UdpRequestsPlugin.java
+++ plugins/capacitor-udp-requests/android/src/main/java/com/bdpsolutions/plugin/UdpRequestsPlugin.java
@@ -3,6 +3,7 @@ package com.bdpsolutions.plugin;
 import android.Manifest;
 
 import com.bdpsolutions.plugin.sender.Sender;
+import com.bdpsolutions.plugin.server.UdpVideoServer;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.PermissionState;
 import com.getcapacitor.Plugin;
@@ -28,7 +29,7 @@ permissions ={
 public class UdpRequestsPlugin extends Plugin {
 
     private UdpRequests implementation = new UdpRequests();
-
+    private UdpVideoServer videoServer;
     @PluginMethod
     public void echo(PluginCall call) {
         String value = call.getString("value");
@@ -66,11 +67,11 @@ public class UdpRequestsPlugin extends Plugin {
     }
 
     private void startVideoStreamServer(){
-
+        videoServer = new UdpVideoServer();
     }
 
     private void stopVideoStreamServer(){
-
+        videoServer.stopServer();
     }
 
 
