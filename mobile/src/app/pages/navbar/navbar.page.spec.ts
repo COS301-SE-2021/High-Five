894ed84858df296765973a448562c64aeda763fe
darthcerellius <71949550+darthcerellius@users.noreply.github.com>
2021-06-02 16:15:55 +0200

    Mobile feature videoplayback (#35)
    * Added some unit tests - Kyle Barry (u19232510)
    
    * Merged mobile-development - Kyle Barry (u19232510)
    
    * WIP on writing unit tests for VideostorecardComponent - Kyle Barry (u19232510)
    
    * Finished unit testing for videostorecard - Kyle Barry (u19232510)
    
    * Added unit tests for register card - Kyle Barry (u19232510)

--- mobile/src/app/pages/navbar/navbar.page.spec.ts
+++ mobile/src/app/pages/navbar/navbar.page.spec.ts
@@ -2,15 +2,25 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { NavbarPage } from './navbar.page';
+import {Router} from "@angular/router";
+
+let RouterMock;
 
 describe('NavbarPage', () => {
   let component: NavbarPage;
   let fixture: ComponentFixture<NavbarPage>;
 
+  RouterMock = {
+    navigate: jasmine.createSpy('navigate')
+  }
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NavbarPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [],
+      providers: [
+        {provide: Router, useValue: RouterMock}
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarPage);
@@ -21,4 +31,16 @@ describe('NavbarPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to videos', () => {
+    let component = TestBed.createComponent(NavbarPage).componentInstance;
+    component.navigateTo("navbar/videos", "videoLink");
+    expect(RouterMock.navigate).toHaveBeenCalledWith(["navbar/videos"]);
+  })
+
+  it('should go to analytics', () => {
+    let component = TestBed.createComponent(NavbarPage).componentInstance;
+    component.navigateTo("navbar/analytics", "videoLink");
+    expect(RouterMock.navigate).toHaveBeenCalledWith(["navbar/analytics"]);
+  })
 });
