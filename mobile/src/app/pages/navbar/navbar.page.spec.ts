e69b28fd6e0175d737927f46997219f09a37e02d
Kyle Barry <u19232510@tuks.co.za>
2021-06-02 22:04:52 +0200

    Removed duplications - Kyle Barry (u19232510)

--- mobile/src/app/pages/navbar/navbar.page.spec.ts
+++ mobile/src/app/pages/navbar/navbar.page.spec.ts
@@ -6,14 +6,23 @@ import {Router} from "@angular/router";
 
 let RouterMock;
 
+/**
+ * Tests the entire test suite for the NavbarPage
+ */
 describe('NavbarPage', () => {
   let component: NavbarPage;
   let fixture: ComponentFixture<NavbarPage>;
 
+  /**
+   * Creates a mock implementation for Ionic's Router object
+   */
   RouterMock = {
     navigate: jasmine.createSpy('navigate')
   }
 
+  /**
+   * Pre-flight code that runs before each test
+   */
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NavbarPage ],
@@ -28,16 +37,27 @@ describe('NavbarPage', () => {
     fixture.detectChanges();
   }));
 
+  /**
+   * Tests that the creation of the component is successful
+   */
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  /**
+   * Tests that the component calls the navigate function and that it passed in the
+   * correct parameter.
+   */
   it('should go to videos', () => {
     let component = TestBed.createComponent(NavbarPage).componentInstance;
     component.navigateTo("navbar/videos", "videoLink");
     expect(RouterMock.navigate).toHaveBeenCalledWith(["navbar/videos"]);
   })
 
+  /**
+   * Tests that the component calls the navigate function and that it passed in the
+   * correct parameter.
+   */
   it('should go to analytics', () => {
     let component = TestBed.createComponent(NavbarPage).componentInstance;
     component.navigateTo("navbar/analytics", "videoLink");
