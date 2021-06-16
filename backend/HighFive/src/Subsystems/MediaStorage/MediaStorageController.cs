ad66a4a1b299ca61159aca5429a6f1a6516d6f37
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-06-16 16:04:13 +0200

    Added RetrieveVideos endpoint in the Media Storage subsystem

--- backend/HighFive/src/Subsystems/MediaStorage/MediaStorageController.cs
+++ backend/HighFive/src/Subsystems/MediaStorage/MediaStorageController.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Org.OpenAPITools.Controllers;
 using Org.OpenAPITools.Models;
 
 namespace src.Subsystems.MediaStorage
 {
-    [ApiController]
     public class MediaStorageController : MediaStorageApiController
     {
         public override IActionResult StoreVideo(IFormFile file)
@@ -14,6 +14,13 @@ namespace src.Subsystems.MediaStorage
             Response.Message = "Video stored successfully";
             Response.Success = true;
             return StatusCode(200, Response);
+        }
+        
+        public override IActionResult RetrieveVideos()
+        {
+            List<RetrieveVideosResponse> responseList = new List<RetrieveVideosResponse>();
+            
+            return StatusCode(200, responseList.ToArray());
         }
     }
 }
