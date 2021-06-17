dcba4d7ee65bce628ca428db46fc5d0e58ccb00a
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-06-17 17:42:44 +0200

    Renamed openapi.json to swagger.json

--- backend/HighFive/src/Startup.cs
+++ backend/HighFive/src/Startup.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using src.Storage;
 using src.Subsystems.MediaStorage;
 
@@ -25,7 +23,8 @@ namespace src
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "src", Version = "v1"}); });
+            services.AddSwaggerGen();
+            
             // Dependency Injections
             services.Add(new ServiceDescriptor(typeof(IStorageManager), new StorageManager(Configuration)));//singleton
             services.AddScoped<IMediaStorageService, MediaStorageService>();
@@ -38,28 +37,14 @@ namespace src
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseSwagger(c =>
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
                 {
-                    c.RouteTemplate = "openapi/{documentName}/openapi.json";
-                })
-                .UseSwaggerUI(c =>
-                {
-                    // set route prefix to openapi, e.g. http://localhost:8080/openapi/index.html
-                    c.RoutePrefix = "openapi";
-                    //TODO: Either use the SwaggerGen generated OpenAPI contract (generated from C# classes)
-                    c.SwaggerEndpoint("/openapi/0.0.1/openapi.json", "High Five");
-
-                    //TODO: Or alternatively use the original OpenAPI contract that's included in the static files
-                    // c.SwaggerEndpoint("/openapi-original.json", "High Five Original");
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "High Five");
+                    c.RoutePrefix = String.Empty;
                 });
+            app.UseHttpsRedirection();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
