ea61b0ac900d116137bdd4be8f58f9577511dfe9
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-06-18 12:57:54 +0200

    Re-added missing code that was removed during merge

--- backend/HighFive/src/Subsystems/MediaStorage/MediaStorageController.cs
+++ backend/HighFive/src/Subsystems/MediaStorage/MediaStorageController.cs
@@ -18,6 +18,24 @@ namespace src.Subsystems.MediaStorage
             _mediaStorageService = mediaStorageService;
         }
 
+        public override IActionResult GetAllVideos()
+        {
+            var result = _mediaStorageService.GetAllVideos();
+            return StatusCode(200, result);
+        }
+
+        public override IActionResult GetVideo(GetVideoRequest getVideoRequest)
+        {
+            var response = _mediaStorageService.GetVideo(getVideoRequest.Id);
+            if (response.Result != null) return StatusCode(200, response.Result);
+            var fail = new EmptyObject
+            {
+                Success = false,
+                Message = "No video exists associated with video id: " + getVideoRequest.Id
+            };
+            return StatusCode(400, fail);
+        }
+
         public override async Task<IActionResult> StoreVideo(IFormFile file)
         {
             try
