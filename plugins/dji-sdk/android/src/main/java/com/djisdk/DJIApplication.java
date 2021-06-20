fe3d672fe45d679c0b73d9831a3a96b1df163daa
ruan-bieldt <u13145992@tuks.co.za>
2021-06-20 22:56:38 +0200

    Added DJIApplication implementation

--- plugins/dji-sdk/android/src/main/java/com/djisdk/DJIApplication.java
+++ plugins/dji-sdk/android/src/main/java/com/djisdk/DJIApplication.java
@@ -2,13 +2,74 @@ package com.djisdk;
 
 import android.app.Application;
 import android.content.Context;
-
+import androidx.multidex.MultiDex;
 import com.secneo.sdk.Helper;
+import com.squareup.otto.Bus;
+import com.squareup.otto.ThreadEnforcer;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.products.Aircraft;
+import dji.sdk.products.HandHeld;
+import dji.sdk.sdkmanager.BluetoothProductConnector;
+import dji.sdk.sdkmanager.DJISDKManager;
 
 public class DJIApplication extends Application {
+
+    public static final String TAG = DJIApplication.class.getName();
+
+    private static BaseProduct product;
+    private static BluetoothProductConnector bluetoothConnector = null;
+    private static Bus bus = new Bus(ThreadEnforcer.ANY);
+    private static Application app = null;
+
+    /**
+     * Gets instance of the specific product connected after the
+     * API KEY is successfully validated.
+     */
+    public static synchronized BaseProduct getProductInstance() {
+        product = DJISDKManager.getInstance().getProduct();
+        return product;
+    }
+
+    public static synchronized BluetoothProductConnector getBluetoothProductConnector() {
+        bluetoothConnector = DJISDKManager.getInstance().getBluetoothProductConnector();
+        return bluetoothConnector;
+    }
+
+    public static boolean isAircraftConnected() {
+        return getProductInstance() != null && getProductInstance() instanceof Aircraft;
+    }
+
+    public static boolean isHandHeldConnected() {
+        return getProductInstance() != null && getProductInstance() instanceof HandHeld;
+    }
+
+    public static synchronized Aircraft getAircraftInstance() {
+        if (!isAircraftConnected()) {
+            return null;
+        }
+        return (Aircraft) getProductInstance();
+    }
+
+    public static synchronized HandHeld getHandHeldInstance() {
+        if (!isHandHeldConnected()) {
+            return null;
+        }
+        return (HandHeld) getProductInstance();
+    }
+
+    public static Application getInstance() {
+        return DJIApplication.app;
+    }
+
+    public static Bus getEventBus() {
+        return bus;
+    }
+
     @Override
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
-        Helper.install(DJIApplication.this);
+        MultiDex.install(this);
+        com.secneo.sdk.Helper.install(this);
+        app = this;
     }
 }
