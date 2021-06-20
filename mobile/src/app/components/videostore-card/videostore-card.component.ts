b9d219ab0489c61071589519967d2e31bb101b69
Kyle Barry <u19232510@tuks.co.za>
2021-06-20 23:38:39 +0200

    WIP on deleting video. Added toast. - Kyle Barry (u19232510)

--- mobile/src/app/components/videostore-card/videostore-card.component.ts
+++ mobile/src/app/components/videostore-card/videostore-card.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {AlertController, ModalController, Platform} from '@ionic/angular';
+import {AlertController, ModalController, Platform, ToastController} from '@ionic/angular';
 import {VideostreamCardComponent} from '../videostream-card/videostream-card.component';
 import {VideoMetaData} from '../../models/videoMetaData';
 import {VideouploadService} from '../../services/videoupload/videoupload.service';
@@ -13,7 +13,8 @@ export class VideostoreCardComponent implements OnInit {
   @Input() data: VideoMetaData;
 
   constructor(public platform: Platform, private modal: ModalController,
-              private videoService: VideouploadService, private alertController: AlertController) { }
+              private videoService: VideouploadService, private alertController: AlertController,
+              private toastController: ToastController) { }
 
   ngOnInit() {
   }
@@ -61,8 +62,16 @@ export class VideostoreCardComponent implements OnInit {
     const { role } = await alert.onDidDismiss();
 
     if (role === 'yes') {
-      this.videoService.deleteVideo(vidId, data => {
+      this.videoService.deleteVideo(vidId, async data => {
         console.log(data);
+        const toast = await this.toastController.create({
+          cssClass: 'alert-style',
+          header: 'Video Deleted',
+          message: 'Video successfully deleted.',
+          buttons: ['OK']
+        });
+
+        await toast.present();
       });
     }
   }
