4f0d71f82076ad66eceafb9a48db54f50f9fcac8
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-06-20 23:56:50 +0200

    Added code to clear add pipeline component inputs after creation

--- mobile/src/app/components/components.module.ts
+++ mobile/src/app/components/components.module.ts
@@ -8,16 +8,18 @@ import {VgControlsModule} from '@videogular/ngx-videogular/controls';
 import {VgCoreModule} from '@videogular/ngx-videogular/core';
 import {PipelineComponent} from './pipeline/pipeline.component';
 import {AddPipelineComponent} from './add-pipeline/add-pipeline.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [VideostoreCardComponent, VideostreamCardComponent, PipelineComponent, AddPipelineComponent],
-  imports: [
-    CommonModule,
-    IonicModule,
-    RouterModule,
-    VgControlsModule,
-    VgCoreModule,
-  ],
+    imports: [
+        CommonModule,
+        IonicModule,
+        RouterModule,
+        VgControlsModule,
+        VgCoreModule,
+        FormsModule,
+    ],
   exports: [VideostoreCardComponent, VideostreamCardComponent, PipelineComponent, AddPipelineComponent]
 })
 export class CustomComponentsModule {}
