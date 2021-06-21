d13b285be4a83a3ac5db79456c4ec4c4ab86b899
Kyle Barry <u19232510@tuks.co.za>
2021-06-21 12:59:23 +0200

    fixed unit testing for edit-pip eline- Kyle Barry (u19232510)

--- mobile/src/app/components/add-pipeline/add-pipeline.component.spec.ts
+++ mobile/src/app/components/add-pipeline/add-pipeline.component.spec.ts
@@ -2,6 +2,17 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { AddPipelineComponent } from './add-pipeline.component';
+import {PipelinesService} from '../../apis/pipelines.service';
+import {BehaviorSubject} from 'rxjs';
+import {distinctUntilChanged} from 'rxjs/operators';
+
+const mockPipelinesService = jasmine.createSpyObj('PipelinesService', [ 'getPipelines','deletePipeline']);
+mockPipelinesService.getPipelines.and.callFake(
+  ()=>((new BehaviorSubject(false).asObservable().pipe(distinctUntilChanged())))
+);
+mockPipelinesService.deletePipeline.and.callFake(
+  (piplineId='id')=>((new BehaviorSubject(false).asObservable().pipe(distinctUntilChanged())))
+);
 
 describe('AddPipelineComponent', () => {
   let component: AddPipelineComponent;
@@ -10,7 +21,8 @@ describe('AddPipelineComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AddPipelineComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [{provide: PipelinesService, useValue: mockPipelinesService}]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddPipelineComponent);
