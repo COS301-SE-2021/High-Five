50c0237daad7ac0bad2bfc135a43c3df875976d5
NJHKirsten <u19049014@tuks.co.za>
2021-07-02 13:53:59 +0200

    Created new tool template

---
+++ backend/HighFive/src/AnalysisTools/ConcreteTools/ObjectRecognition.cs
@@ -0,0 +1,11 @@
+namespace src.Subsystems.MediaStorage
+{
+    public class ObjectRecognition
+    {
+        public byte[] ProcessFrame(byte[] frame)
+        {
+            
+            return {1,0,0,1}
+        }
+    }
+}
