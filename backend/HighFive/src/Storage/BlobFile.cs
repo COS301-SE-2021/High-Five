9e41650b7aa1e323c8896fc4196636c2cb89a662
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-07-04 15:21:39 +0200

    Updated description and parameter commentary for the existing functions in BlobFile.cs

--- backend/HighFive/src/Storage/BlobFile.cs
+++ backend/HighFive/src/Storage/BlobFile.cs
@@ -8,9 +8,13 @@ using Microsoft.WindowsAzure.Storage.Blob;
 namespace src.Storage
 {
     /*
+     *      Description:
      * This is a wrapper class for Azure SDK's CloudBlockBlob object, which will contain the data of a file
      * retrieved from Azure Blob Storage. Usage of this class prohibits the services from modifying, saving
      * or deleting any data from the CloudBlockBlob in an unprotected manner.
+     *
+     * Attributes:
+     * _file - this is a reference to the Blob from storage that is currently being handled
      */
     public class BlobFile
     {
@@ -20,21 +24,33 @@ namespace src.Storage
         {
             _file = file;
         }
-
-        /*
-         * The AddMetaData function adds a key-value pair as meta-data to the blob file.
-         *
-         * Parameters:
-         * key - this parameter represents the key in the key-value pair being added as meta-data
-         * value - this parameter represents the value in the key-value pair being added as meta-data
-         */
+        
         public void AddMetadata(string key, string value)
         {
+            /*
+             *      Description:
+             * The AddMetaData function adds a key-value pair as meta-data to the blob file.
+             *
+             *      Parameters:
+             * key - this parameter represents the key in the key-value pair being added as meta-data
+             * value - this parameter represents the value in the key-value pair being added as meta-data
+             */
+            
             _file.Metadata.Add(new KeyValuePair<string, string>(key, value));;
         }
         
         public async Task Upload(IFormFile newFile)
         {
+            /*
+             *      Description:
+             * The Upload function will upload a new file to the Blob in the Azure Blob Storage assosciated
+             * with the file contained within this BlobFile. This function overwrites any data currently stored
+             * in the CloudBlockBlob file and uploads it to the storage directly.
+             *
+             *      Parameters:
+             * newFile - this parameter is the new file to be uploaded to the blob storage
+             */
+            
             var ms = new MemoryStream();
             await newFile.CopyToAsync(ms);
             var fileBytes = ms.ToArray();
