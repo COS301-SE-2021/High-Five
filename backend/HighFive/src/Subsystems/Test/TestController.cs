8326de395c58a3d5858bd081ed0523888cf95dd1
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-07-04 13:51:26 +0200

    Incorporated startup code for Azure AD B2C authentication.

--- backend/HighFive/src/Subsystems/Test/TestController.cs
+++ backend/HighFive/src/Subsystems/Test/TestController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Org.OpenAPITools.Controllers;
 using Org.OpenAPITools.Models;
 
 namespace src.Subsystems.Test
 {
+    [Authorize]
     public class TestController: TestApiController
     {
         public override IActionResult Echo(EchoRequest echoRequest)
