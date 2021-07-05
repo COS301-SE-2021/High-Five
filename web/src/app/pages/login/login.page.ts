df0608675543f42007d2bc5b771c4bebf0c68870
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-07-05 17:34:20 +0200

    Added models for login page inputs

--- web/src/app/pages/login/login.page.ts
+++ web/src/app/pages/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
