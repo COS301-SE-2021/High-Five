9d887a2db1daf067e6fc1d8f5db38a7ef9337f2c
Kyle Barry <u19232510@tuks.co.za>
2021-07-07 09:59:23 +0200

    Created a splash screen - Kyle Barry (u19232510)

---
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.bdpsolutions.highfive
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.bdpsolutions.highfive.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        // Hiding status bars, since this is the splash screen
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            // Hiding status bar for older versions of Android.
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            )
+        }
+    }
+}
