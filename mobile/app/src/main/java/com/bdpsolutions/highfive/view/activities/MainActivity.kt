c78308b5fcf1a71249c42d145d6b8c351ff36192
Kyle Barry <u19232510@tuks.co.za>
2021-07-08 13:18:58 +0200

    Added video fragment and fixed content placement issue - Kyle Barry (u19232510)

--- mobile/app/src/main/java/com/bdpsolutions/highfive/view/activities/MainActivity.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/view/activities/MainActivity.kt
@@ -3,7 +3,7 @@ package com.bdpsolutions.highfive.view.activities
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -20,17 +20,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController = (supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment_activity_main) as NavHostFragment).navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_analysis, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_analysis, R.id.navigation_video
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //binding.marginBinding = MainLayoutDataBinding(binding)
     }
 }
