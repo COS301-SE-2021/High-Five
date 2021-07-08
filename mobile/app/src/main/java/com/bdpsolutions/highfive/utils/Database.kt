be9e8507ca6e77a3046815e2832be2dbb9347ea1
darthcerellius <71949550+darthcerellius@users.noreply.github.com>
2021-07-08 11:14:32 +0200

    Mobile refactoring (#104)
    * Did major refactoring to conform to the MVVM architecture - Kyle Barry (u19232510)
    
    * Did major refactoring to conform to the MVVM architecture - Kyle Barry (u19232510)

--- mobile/app/src/main/java/com/bdpsolutions/highfive/utils/Database.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/utils/Database.kt
@@ -2,8 +2,8 @@ package com.bdpsolutions.highfive.utils
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.bdpsolutions.highfive.data.login.model.User
-import com.bdpsolutions.highfive.data.login.model.UserDao
+import com.bdpsolutions.highfive.models.login.model.User
+import com.bdpsolutions.highfive.models.login.model.UserDao
 
 @Database(entities = [User::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
