500648552af0cd8c8b0b3402176287bb97969b14
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-07-11 10:53:01 +0200

    Added packages for integration testing, as well as a Startup file that bypasses authentication during integration testing and enables mock storage. Added a single test function in MediaStorageIntegrationTests.cs.

---
+++ backend/HighFive/tests/IntegrationTests/MediaStorageIntegrationTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using src;
+using src.Storage;
+using src.Subsystems.MediaStorage;
+using src.Subsystems.Pipelines;
+using Xunit;
+
+namespace tests.IntegrationTests
+{
+    public class MediaStorageControllerTests
+    {
+        /*
+         *      Description:
+         * This class runs integration tests on the MediaStorage subsystem controller with various
+         * combinations of valid and invalid inputs.
+         *
+         *      Attributes:
+         * -> _server: the test server that will be used to run the integration tests.
+         * -> _client: the client that will run the tests on the test server.
+         */
+
+        private TestServer _server;
+        private HttpClient _client;
+
+        public MediaStorageControllerTests()
+        {
+            /*
+             *      Description:
+             * The constructor for this integration testing class instantiates the server and a client
+             * through ASP.NET Core's built-in webhost.
+             */
+
+            _server = new TestServer(new WebHostBuilder()
+                .UseTestServer()
+                .UseStartup<TestStartup>());
+            _client = _server.CreateClient();
+        }
+        
+        [Fact]
+        public async Task Test()
+        {
+            var response = await _client.PostAsync("/test/ping", null!);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var responseCode = response.StatusCode;
+        }
+        
+    }
+}
