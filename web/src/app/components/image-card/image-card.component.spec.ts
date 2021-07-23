e28073aa114761c68e86fd8390552c9f2d327398
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-07-23 11:26:39 +0200

    Added more unit tests to image-card.component.spec.ts

--- web/src/app/components/image-card/image-card.component.spec.ts
+++ web/src/app/components/image-card/image-card.component.spec.ts
@@ -26,14 +26,14 @@ describe('ImageCardComponent', () => {
   }));
 
   /**
-   * Ensures the component has been created successfully
+   * Checks the component has been created successfully
    */
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   /**
-   * Ensures that the image will have the correct src and will therefore display the correct image
+   * Checks that the image will have the correct src and will therefore display the correct image
    */
   it('should show correct image', ()=>{
     const imageUrl = fixture.debugElement.nativeElement.querySelector('ion-img').src;
@@ -41,7 +41,7 @@ describe('ImageCardComponent', () => {
   });
 
   /**
-   * Ensures that the image tittle matches the mock image model's title
+   * Checks that the image tittle matches the mock image model's title
    */
   it('should show correct title', ()=>{
     const cardTitle = fixture.debugElement.nativeElement.querySelector('ion-card-title').innerText.trim();
@@ -49,12 +49,54 @@ describe('ImageCardComponent', () => {
   });
 
   /**
-   * Ensures that the image's delete button calls the correct method
+   * Checks that the image's delete button calls the correct method
    */
   it('should call onDeleteImage method', ()=>{
     spyOn(component,'onDeleteImage');
     const deleteButton = fixture.debugElement.nativeElement.querySelectorAll('ion-button')[1];
     deleteButton.click();
     expect(component.onDeleteImage).toHaveBeenCalled();
+  });
+
+  /**
+   * Checks that the component's delete button emits the deleteImage event
+   */
+  it('should emit the deleteImage event on click', ()=>{
+    spyOn(component.deleteImage,'emit');
+    const deleteButton = fixture.debugElement.nativeElement.querySelectorAll('ion-button')[1];
+    deleteButton.click();
+    expect(component.deleteImage.emit).toHaveBeenCalled();
+  });
+
+  /**
+   * Checks that the deleteImage event contains the correct payload once it's emitted
+   */
+  it('deleteImage event should contain correct payload',  () =>{
+    spyOn(component.deleteImage,'emit');
+    const deleteButton = fixture.debugElement.nativeElement.querySelectorAll('ion-button')[1];
+    deleteButton.click();
+    expect(component.deleteImage.emit).toHaveBeenCalledWith(mockImageModel.id);
+  });
+
+  /**
+   * Checks that the analyse image button press calls the correct function in the component
+   */
+  it('should call correct component function once pressed',  ()=> {
+    spyOn(component,'analyseImage');
+    const deleteButton = fixture.debugElement.nativeElement.querySelectorAll('ion-button')[0];
+    deleteButton.click();
+    expect(component.analyseImage).toHaveBeenCalled();
+
+  });
+
+
+  /**
+   * Checks that the analyse image button press changes the analysed value of the image model
+   */
+  it('should change analysed value of image model',  ()=> {
+    const analysed = component.image.analysed;
+    component.analyseImage();
+    expect(component.image.analysed).toBe(!mockImageModel.analysed);
+
   });
 });
