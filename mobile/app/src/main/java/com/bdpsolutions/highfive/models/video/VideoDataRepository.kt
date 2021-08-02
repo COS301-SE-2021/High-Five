3f77dddd7f76ee25aa3f35dd90d3119cdfba5b21
Kyle Barry <u19232510@tuks.co.za>
2021-08-02 11:30:26 +0200

    Updated Gradle scripts - Kyle Barry (u19232510)

--- mobile/app/src/main/java/com/bdpsolutions/highfive/models/video/VideoDataRepository.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/models/video/VideoDataRepository.kt
@@ -1,13 +1,35 @@
 package com.bdpsolutions.highfive.models.video
 
-import android.media.MediaRecorder
 import com.bdpsolutions.highfive.models.video.model.VideoPreview
 import com.bdpsolutions.highfive.models.video.source.VideoDataSource
 import com.bdpsolutions.highfive.utils.Result
 
+/**
+ * @author Kyle Barry (u19232510@tuks.co.za)
+ *
+ * Repository class to access Video data.
+ *
+ * Utilises two types of sources: an API source and a Database source.
+ * The API source is for fetching new data from the backend, while the Database
+ * source is used for caching data so that API calls do not have to be made
+ * every time the view is loaded.
+ *
+ * As API calls use the Volley library, this class takes in callback functions instead of
+ * returning data, as Volley takes callback functions and passes the data to the function.
+ *
+ * To remain consistent, database access will use the same behaviour.
+ *
+ * @param apiVideoSource API endpoints to access data
+ * @param dbVideoSource Database endpoints to access data
+ */
 class VideoDataRepository(private val apiVideoSource: VideoDataSource,
                           private val dbVideoSource: VideoDataSource) {
 
+    /**
+     * Fetches new video preview data from the backend service.
+     *
+     * @param callback A callback function that takes a list of video preview data as an argument.
+     */
     fun refreshData(callback: (Result<List<VideoPreview>>) -> Unit) {
         apiVideoSource.getVideoPreviewData(callback)
     }
