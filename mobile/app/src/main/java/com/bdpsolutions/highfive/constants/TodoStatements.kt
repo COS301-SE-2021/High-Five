746cf2525466a2907076cbe85a59c99dc07e4eef
Kyle Barry <u19232510@tuks.co.za>
2021-08-02 11:41:33 +0200

    Added Todo statements to fix code smell issue. - Kyle Barry (u19232510)

---
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/constants/TodoStatements.kt
@@ -0,0 +1,5 @@
+package com.bdpsolutions.highfive.constants
+
+object TodoStatements {
+    val NOT_YET_IMPLEMENTED: String = "Not yet implemented"
+}
--- mobile/app/src/main/java/com/bdpsolutions/highfive/models/video/source/DatabaseVideoDataSource.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/models/video/source/DatabaseVideoDataSource.kt
@@ -1,18 +1,19 @@
 package com.bdpsolutions.highfive.models.video.source
 
+import com.bdpsolutions.highfive.constants.TodoStatements
 import com.bdpsolutions.highfive.models.video.model.VideoPreview
 import com.bdpsolutions.highfive.utils.Result
 
 class DatabaseVideoDataSource : VideoDataSource {
     override fun getVideoPreviewData(callback: (Result<List<VideoPreview>>) -> Unit) {
-        TODO("Not yet implemented")
+        TODO(TodoStatements.NOT_YET_IMPLEMENTED)
     }
 
     override fun addPreviewData(vararg data: VideoPreview) {
-        TODO("Not yet implemented")
+        TODO(TodoStatements.NOT_YET_IMPLEMENTED)
     }
 
     override fun deletePreviewData(id: String) {
-        TODO("Not yet implemented")
+        TODO(TodoStatements.NOT_YET_IMPLEMENTED)
     }
 }
