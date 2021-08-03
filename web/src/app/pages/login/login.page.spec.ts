b9c504af7c06716e90dba7dc36809b322be36e5c
Jeanré du Plessis <47949489+JayXD-2K@users.noreply.github.com>
2021-08-03 11:35:07 +0200

    Web feature login changes merging into web-development (#128)
    * Basic login page created
    
    * Switched to using components to create login page's card
    
    * Added a margin to the login card's div
    
    * Replaced base64 encoded image string from img src
    
    * Integrated login card component code into login page (removed login-card.component)
    
    * Added models for login page inputs
    
    * Fixed login page unit test fail

--- web/src/app/pages/login/login.page.spec.ts
+++ web/src/app/pages/login/login.page.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('LoginPage', () => {
   let component: LoginPage;
@@ -10,7 +11,7 @@ describe('LoginPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
