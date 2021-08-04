05034c289faddaa4fa005785848cbab2895c12ef
NJHKirsten <u19049014@tuks.co.za>
2021-08-04 14:46:22 +0200

    Added tool interface

--- backend/HighFive/src/AnalysisTools/AnalysisThread/ToolRunner.cs
+++ backend/HighFive/src/AnalysisTools/AnalysisThread/ToolRunner.cs
@@ -1,7 +1,32 @@
+using System.Threading;
+using System.Collections.Concurrent;
+
 namespace src.AnalysisTools.AnalysisThread
 {
     public class ToolRunner
     {
-        
+        private BlockingCollection<object> _frames = new BlockingCollection<object>();
+ 
+        public ToolRunner()
+        {
+            var thread = new Thread(new ThreadStart(OnStart));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+ 
+        public void Enqueue(object job)
+        {
+            _frames.Add(job);
+        }
+ 
+        private void OnStart()
+        {
+            //Load model here
+            foreach (var frame in _frames.GetConsumingEnumerable(CancellationToken.None))
+            {
+                //Analyse images
+                //Add to output queue
+            }
+        }
     }
 }
---
+++ backend/HighFive/src/AnalysisTools/ITool.cs
@@ -0,0 +1,7 @@
+namespace src.AnalysisTools
+{
+    public interface ITool
+    {
+        
+    }
+}
