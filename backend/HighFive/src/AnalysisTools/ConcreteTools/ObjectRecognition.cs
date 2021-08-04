14fd22f1384d3ae3deb4d9f3a4985fe8e5f11643
NJHKirsten <u19049014@tuks.co.za>
2021-08-04 12:59:45 +0200

    Added thread structure for analysis tool

--- backend/HighFive/src/AnalysisTools/ConcreteTools/ObjectRecognition.cs
+++ backend/HighFive/src/AnalysisTools/ConcreteTools/ObjectRecognition.cs
@@ -5,65 +5,39 @@ using Microsoft.Scripting;
 using Microsoft.Scripting.Runtime;  
 using System.IO; 
 
-// using System;
-// using System.Collections.Generic;
-// using System.Drawing;
-// using System.Linq;
-// using Microsoft.ML.OnnxRuntime;
-// using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
 
 namespace src.AnalysisTools.ConcreteTools
 {
     public class ObjectRecognition
     {
-		public static void Start(string[] args)  
-        {  
-            // float[][] image = PreprocessTestImage(imagePath);
-            //
-            // const string modelPath = @"mnist-model.onnx";
-            // float[] probabilities = Predict(modelPath, image);
-        }
-        
-        private static float[][] PreprocessTestImage(string path)
+        private BlockingCollection<object> _frames = new BlockingCollection<object>();
+ 
+        public ObjectRecognition()
         {
-            // var img = new Bitmap(path);
-            // var result = new float[img.Width][];
-            //
-            // for (int i = 0; i < img.Width; i++)
-            // {
-            //     result[i] = new float[img.Height];
-            //     for (int j = 0; j < img.Height; j++)
-            //     {
-            //         var pixel = img.GetPixel(i, j);
-            //         
-            //         var gray = RgbToGray(pixel);
-            //         
-            //         // Normalize the Gray value to 0-1 range
-            //         var normalized = gray / 255;
-            //         
-            //         result[i][j] = normalized;
-            //     }
-            // }
-            // return result;
-            return null;
+            var thread = new Thread(new ThreadStart(OnStart));
+            thread.IsBackground = true;
+            thread.Start();
         }
-        
-        private static float[] Predict(string modelPath, float[][] image)
+ 
+        public void Enqueue(object job)
         {
-            // using var session = new InferenceSession(modelPath);
-            // var modelInputLayerName = session.InputMetadata.Keys.Single();
-            //
-            // var imageFlattened = image.SelectMany(x => x).ToArray();
-            // int[] dimensions = {1, 28, 28};
-            // var inputTensor = new DenseTensor<float>(imageFlattened, dimensions);
-            // var modelInput = new List<NamedOnnxValue>
-            // {
-            //     NamedOnnxValue.CreateFromTensor(modelInputLayerName, inputTensor)
-            // };
-            //
-            // var result = session.Run(modelInput);
-            // return ((DenseTensor<float>) result.Single().Value).ToArray();
-            return null;
+            _frames.Add(job);
+        }
+ 
+        private void OnStart()
+        {
+            foreach (var frame in _frames.GetConsumingEnumerable(CancellationToken.None))
+            {
+                Console.WriteLine(frame);
+            }
         }
     }
 }
