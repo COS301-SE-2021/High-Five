ddd68220756b6732b4cf3ebc91083923a347bc15
NJHKirsten <u19049014@tuks.co.za>
2021-08-04 10:41:51 +0200

    Added functions for using analysis model

--- backend/HighFive/src/AnalysisTools/ConcreteTools/ObjectRecognition.cs
+++ backend/HighFive/src/AnalysisTools/ConcreteTools/ObjectRecognition.cs
@@ -5,33 +5,64 @@ using Microsoft.Scripting;
 using Microsoft.Scripting.Runtime;  
 using System.IO; 
 
-namespace src.Subsystems.MediaStorage
+// using System;
+// using System.Collections.Generic;
+// using System.Drawing;
+// using System.Linq;
+// using Microsoft.ML.OnnxRuntime;
+// using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace src.AnalysisTools.ConcreteTools
 {
     public class ObjectRecognition
     {
 		public static void Start(string[] args)  
         {  
-            //// Creates the python runtime   
-            ScriptRuntime ipy = Python.CreateRuntime();  
-            //// The dynamic keyword is needed   
-            //// here because Python is an interpreted   
-            //// scripting language where calls are bound   
-            //// at run time, not at compile time.   
-            //// There is no simple or practical way to   
-            //// bind calls to Python at compile time   
-            //// because Python is designed to be a   
-            //// dynamic language which is resolved   
-            //// at runtime.   
-            dynamic test1 = ipy.UseFile("main.py");  
-            // Call function   
-            string result=test1.analyse();  
-            Console.WriteLine(result);
-        } 
-		
-        public byte[] ProcessFrame(byte[] frame)
+            // float[][] image = PreprocessTestImage(imagePath);
+            //
+            // const string modelPath = @"mnist-model.onnx";
+            // float[] probabilities = Predict(modelPath, image);
+        }
+        
+        private static float[][] PreprocessTestImage(string path)
         {
-            // IntPtr image= CvInvoke.imread
-            return Array.Empty<byte>();
+            // var img = new Bitmap(path);
+            // var result = new float[img.Width][];
+            //
+            // for (int i = 0; i < img.Width; i++)
+            // {
+            //     result[i] = new float[img.Height];
+            //     for (int j = 0; j < img.Height; j++)
+            //     {
+            //         var pixel = img.GetPixel(i, j);
+            //         
+            //         var gray = RgbToGray(pixel);
+            //         
+            //         // Normalize the Gray value to 0-1 range
+            //         var normalized = gray / 255;
+            //         
+            //         result[i][j] = normalized;
+            //     }
+            // }
+            // return result;
+            return null;
+        }
+        
+        private static float[] Predict(string modelPath, float[][] image)
+        {
+            // using var session = new InferenceSession(modelPath);
+            // var modelInputLayerName = session.InputMetadata.Keys.Single();
+            //
+            // var imageFlattened = image.SelectMany(x => x).ToArray();
+            // int[] dimensions = {1, 28, 28};
+            // var inputTensor = new DenseTensor<float>(imageFlattened, dimensions);
+            // var modelInput = new List<NamedOnnxValue>
+            // {
+            //     NamedOnnxValue.CreateFromTensor(modelInputLayerName, inputTensor)
+            // };
+            //
+            // var result = session.Run(modelInput);
+            // return ((DenseTensor<float>) result.Single().Value).ToArray();
         }
     }
 }
