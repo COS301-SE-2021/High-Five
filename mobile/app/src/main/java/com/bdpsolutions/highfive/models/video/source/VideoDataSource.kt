a35f9dc7e2011b56d083c2df05ddfeb60551903b
Kyle Barry <u19232510@tuks.co.za>
2021-08-04 11:55:49 +0200

    Added comments for default implementations. - Kyle Barry (u19232510)

--- mobile/app/src/main/java/com/bdpsolutions/highfive/models/video/source/VideoDataSource.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/models/video/source/VideoDataSource.kt
@@ -5,6 +5,14 @@ import com.bdpsolutions.highfive.utils.Result
 
 interface VideoDataSource {
     fun getVideoPreviewData(callback: (Result<List<VideoPreview>>) -> Unit)
-    fun addPreviewData(vararg data: VideoPreview) {}
-    fun deletePreviewData(id: String) {}
+    fun addPreviewData(vararg data: VideoPreview) {
+        // Default implementation. This function is not needed by all child classes,
+        // So the implementation is blank so that the child does not have to override
+        // This function
+    }
+    fun deletePreviewData(id: String) {
+        // Default implementation. This function is not needed by all child classes,
+        // So the implementation is blank so that the child does not have to override
+        // This function
+    }
 }
