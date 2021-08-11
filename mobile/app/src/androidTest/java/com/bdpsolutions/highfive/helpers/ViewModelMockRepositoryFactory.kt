8ca18ca6818e9497b7dac7eee9d827723c657d0d
darthcerellius <71949550+darthcerellius@users.noreply.github.com>
2021-08-11 11:05:54 +0200

    Mobile development (#143)
    * Mobile feature videoplayback (#117)
    
    * Added video fragment and fixed content placement issue - Kyle Barry (u19232510)
    
    * Added video adapter, as well as video item fragment - Kyle Barry (u19232510)
    
    * Added a video data class to store information about the video - Kyle Barry (u19232510)
    
    * Added Dependency Injection for Recycler view adapter - Kyle Barry (u19232510)
    
    * Removed unnecessary DI classes - Kyle Barry (u19232510)
    
    * Adding video data source - Kyle Barry (u19232510)
    
    * Video data can now be retrieved from the backend API - Kyle Barry (u19232510)
    
    * Updated Gradle scripts - Kyle Barry (u19232510)
    
    * Added Todo statements to fix code smell issue. - Kyle Barry (u19232510)
    
    * Modified Gradle script to include up to date libraries and moved dependency versions to declarations. - Kyle Barry (u19232510)
    
    * Moved over from Volley to Retrofit. - Kyle Barry (u19232510)
    
    * Added comments to VideoDataSource files. - Kyle Barry (u19232510)
    
    * Added comments to VideoDataSource files. - Kyle Barry (u19232510)
    
    * Added unit tests. - Kyle Barry (u19232510)
    
    * Added unit test for Json converter factory. - Kyle Barry (u19232510)
    
    * Added main unit testing class. - Kyle Barry (u19232510)
    
    * Added NOSONAR flags to unit test functions. - Kyle Barry (u19232510)
    
    * Added NOSONAR flags to unit test functions. - Kyle Barry (u19232510)
    
    * Added comments for default implementations. - Kyle Barry (u19232510)
    
    * Added view model for video fragment. - Kyle Barry (u19232510)
    
    * Switched app from bottom navigation to side navigation. - Kyle Barry (u19232510)
    
    * PKG
    
    * Mobile feature login (#142)
    
    * Added Microsoft Azure authentication libraries. - Kyle Barry (u19232510)
    
    * Added code to login via Azure AD - Kyle Barry (u19232510)
    
    * Added code to login via Azure AD - Kyle Barry (u19232510)
    
    * Added code to login via Azure AD - Kyle Barry (u19232510)
    
    * Performed major refactor and added OpenID AppAuth library for signing in users - Kyle Barry (u19232510)
    
    * Performed major refactor and added OpenID AppAuth library for signing in users - Kyle Barry (u19232510)

---
+++ mobile/app/src/androidTest/java/com/bdpsolutions/highfive/helpers/ViewModelMockRepositoryFactory.kt
@@ -0,0 +1,41 @@
+package com.bdpsolutions.highfive.helpers
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bdpsolutions.highfive.subsystems.login.model.LoginRepository
+import com.bdpsolutions.highfive.subsystems.login.model.dataclass.User
+import com.bdpsolutions.highfive.subsystems.login.model.source.APILogin
+import com.bdpsolutions.highfive.subsystems.video.model.VideoDataRepository
+import com.bdpsolutions.highfive.subsystems.video.model.source.APIVideoDataSource
+import com.bdpsolutions.highfive.utils.Result
+import com.bdpsolutions.highfive.subsystems.login.viewmodel.LoginViewModel
+import com.bdpsolutions.highfive.subsystems.video.viewmodel.VideoViewModel
+import org.mockito.Mockito.*
+import org.mockito.Mockito.`when`
+
+class ViewModelMockRepositoryFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+
+            val mockAPI = mock(APILogin::class.java)
+
+//            `when`(mockAPI.login("hello", "world")).thenReturn(
+//                Result.Success(User(0, "xxxx"))
+//            )
+
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    loginSource = mockAPI
+                )
+            ) as T
+        }
+        else if (modelClass.isAssignableFrom(VideoViewModel::class.java)) {
+            return VideoViewModel(
+                VideoDataRepository(APIVideoDataSource(), APIVideoDataSource())
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
