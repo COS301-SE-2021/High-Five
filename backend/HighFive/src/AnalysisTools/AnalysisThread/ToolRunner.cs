79f84a04d002ac44b877eae2a6b9a9ca5b9c963d
NJHKirsten <u19049014@tuks.co.za>
2021-08-13 12:36:18 +0200

    Changed analysis thread structure

--- backend/HighFive/src/AnalysisTools/AnalysisThread/ToolRunner.cs
+++ backend/HighFive/src/AnalysisTools/AnalysisThread/ToolRunner.cs
@@ -1,18 +1,19 @@
 using System.Threading;
 using System.Collections.Concurrent;
+using src.Subsystems.Analysis;
 
 namespace src.AnalysisTools.AnalysisThread
 {
     public class ToolRunner
     {
         private BlockingCollection<byte[]> _frames = new();
-        private ITool Tool;
+        private AnalysisModels _tools;
         private object _outputQueue;//Type may change
  
-        public ToolRunner(ITool tool, object outputQueue)
+        public ToolRunner(AnalysisModels tools, BlockingCollection<byte[]> outputQueue)
         {
             _outputQueue = outputQueue;
-            Tool = tool;
+            _tools = tools;
             var thread = new Thread(new ThreadStart(OnStart));
             thread.IsBackground = true;
             thread.Start();
@@ -30,7 +31,7 @@ namespace src.AnalysisTools.AnalysisThread
             foreach (var frame in _frames.GetConsumingEnumerable(CancellationToken.None))
             {
                 //Analyse images
-                var analysedFrame=Tool.AnalyseFrame(frame);
+                // var analysedFrame=Tool.AnalyseFrame(frame);
                 //Add to output queue
                 //OutputQueue.Enqueue(analysedFrame);
             }
