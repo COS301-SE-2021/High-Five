3ce5e2504772aa7f03068fb65a6872872043c447
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-08-14 11:16:47 +0200

    Adding swiper to be used on landing page

--- web/src/app/pages/landing/landing.module.ts
+++ web/src/app/pages/landing/landing.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { LandingPageRoutingModule } from './landing-routing.module';
 
 import { LandingPage } from './landing.page';
+import {SwiperModule} from "swiper/angular";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    LandingPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        LandingPageRoutingModule,
+        SwiperModule
+    ],
   declarations: [LandingPage]
 })
 export class LandingPageModule {}
--- web/src/app/pages/landing/landing.page.ts
+++ web/src/app/pages/landing/landing.page.ts
@@ -1,17 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterContentChecked, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {SwiperComponent} from "swiper/angular";
+import {SwiperOptions} from "swiper";
+import SwiperCore, {Pagination} from 'swiper/core';
 
+SwiperCore.use([Pagination]);
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.page.html',
   styleUrls: ['./landing.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class LandingPage implements OnInit {
-
+export class LandingPage implements OnInit, AfterContentChecked {
+  @ViewChild('swiper') swiper: SwiperComponent;
+  swiperConfig: SwiperOptions= {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    pagination: true
+  }
   constructor() {
     //Nothing added here yet
   }
 
   ngOnInit() {
     //Nothing added here yet
+  }
+
+  ngAfterContentChecked(): void {
+    if(this.swiper){
+      this.swiper.updateSwiper({});
+    }
   }
 }
