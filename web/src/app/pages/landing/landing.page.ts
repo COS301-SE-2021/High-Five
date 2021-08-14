c495bbfe0f0dd96ad19348f3e13eea89f8c3ffad
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-08-14 15:32:43 +0200

    Updated the slides on the landing page

--- web/src/app/pages/landing/landing.page.ts
+++ web/src/app/pages/landing/landing.page.ts
@@ -1,7 +1,7 @@
 import {AfterContentChecked, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {SwiperComponent} from "swiper/angular";
 import {SwiperOptions} from "swiper";
-import SwiperCore, {Pagination, Mousewheel, Navigation, Autoplay} from 'swiper/core';
+import SwiperCore, {Pagination, Mousewheel, Navigation, Autoplay } from 'swiper/core';
 import {SwiperEvents} from "swiper/types";
 
 SwiperCore.use([Pagination, Mousewheel, Navigation, Autoplay]);
@@ -23,7 +23,7 @@ export class LandingPage implements OnInit, AfterContentChecked {
     loop: true,
     allowTouchMove: false,
     autoplay: {
-      delay: 6000,
+      delay: 12000,
       disableOnInteraction: true,
     }
   }
