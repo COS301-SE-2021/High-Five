736458a1c667b8ffe549c015dd4fbc7e4da24e5c
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-08-14 15:23:07 +0200

    Addded set and get context overrides to DroneApplication.kt

--- mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneApplication.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneApplication.kt
@@ -1,7 +1,17 @@
 package com.bdpsolutions.highfive.subsystems.drone
 
 import android.app.Application
+import android.content.Context
 
 class DroneApplication: Application() {
+    private var instance: Application? = null
+    fun setContext(application: Application?) {
+        instance = application
+    }
 
+    override fun getApplicationContext(): Context? {
+        return instance
+    }
+
+    fun DemoApplication() {}
 }
