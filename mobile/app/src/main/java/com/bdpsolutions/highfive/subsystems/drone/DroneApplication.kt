c040021432ea6a0f979a24c6c7e521b712c7f209
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-08-14 15:37:00 +0200

    Added onCreate override with permission checks

--- mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneApplication.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneApplication.kt
@@ -1,8 +1,11 @@
 package com.bdpsolutions.highfive.subsystems.drone
 
+import android.Manifest
 import android.app.Application
 import android.content.Context
 import android.os.Handler
+import android.os.Looper
+import androidx.core.content.ContextCompat
 import dji.sdk.base.BaseProduct
 import dji.sdk.sdkmanager.DJISDKManager
 import dji.sdk.sdkmanager.DJISDKManager.SDKManagerCallback
@@ -37,5 +40,21 @@ class DroneApplication: Application() {
             mProduct = DJISDKManager.getInstance().product
         }
         return mProduct
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mHandler = Handler(Looper.getMainLooper())
+
+        //Check the permissions before registering the application for android system 6.0 above.
+        val permissionCheck = ContextCompat.checkSelfPermission(
+            applicationContext!!,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        val permissionCheck2 = ContextCompat.checkSelfPermission(
+            applicationContext!!,
+            Manifest.permission.READ_PHONE_STATE
+        )
+
     }
 }
