11780bbc24d54c94dd547bfd331de7371ca3fea3
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-08-14 15:15:31 +0200

    Created drone activity

--- mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneActivity.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneActivity.kt
@@ -1,4 +1,39 @@
 package com.bdpsolutions.highfive.subsystems.drone
 
-class DroneActivity {
+import android.Manifest
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.bdpsolutions.highfive.R
+
+class DroneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // When the compile and target version is higher than 22, please request the
+        // following permissions at runtime to ensure the
+        // SDK work well.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.VIBRATE,
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.WAKE_LOCK,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.READ_PHONE_STATE
+                ), 1
+            )
+        }
+
+        setContentView(R.layout.activity_drone)
+    }
 }
