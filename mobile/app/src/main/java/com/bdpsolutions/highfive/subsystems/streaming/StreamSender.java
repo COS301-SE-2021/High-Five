78c6912038c32c2cc28e3f3a1541f0d601636cd8
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-08-15 17:36:07 +0200

    Created stream sender interface

---
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/streaming/StreamSender.java
@@ -0,0 +1,11 @@
+package com.bdpsolutions.highfive.subsystems.streaming;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public interface StreamSender extends Runnable{
+    static ConcurrentLinkedQueue<byte[]> inputBuffer = new ConcurrentLinkedQueue<>();
+
+    static void pushToBuffer(byte[] data){
+        inputBuffer.add(data);
+    }
+}
