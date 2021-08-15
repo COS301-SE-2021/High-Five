74110c8c69edbe364dedd80ac34a639e4de216e1
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-08-15 18:08:06 +0200

    Added bindChannel function to datagram builder

--- mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/streaming/udp/DatagramChannelBuilder.java
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/streaming/udp/DatagramChannelBuilder.java
@@ -1,11 +1,22 @@
 package com.bdpsolutions.highfive.subsystems.streaming.udp;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.io.IOException;
+import java.net.SocketAddress;
 import java.nio.channels.DatagramChannel;
 
 public class DatagramChannelBuilder {
+
     public static DatagramChannel openChannel() throws IOException {
         DatagramChannel datagramChannel = DatagramChannel.open();
         return datagramChannel;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static DatagramChannel bindChannel(SocketAddress local) throws IOException {
+        return openChannel().bind(local);
     }
 }
