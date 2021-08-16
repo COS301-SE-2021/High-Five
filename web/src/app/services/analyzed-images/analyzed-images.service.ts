a0b2f762732671821bdcf1bd20d5373d39a9eb66
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-08-16 21:14:32 +0200

    Store for analyzed images created

--- web/src/app/services/analyzed-images/analyzed-images.service.ts
+++ web/src/app/services/analyzed-images/analyzed-images.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {MediaStorageService} from "../../apis/mediaStorage.service";
+import {AnalyzedImageMetaData} from "../../models/analyzedImageMetaData";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyzedImagesService {
 
-  constructor() { }
+  private readonly _analyzedImages = new BehaviorSubject<AnalyzedImageMetaData[]>([])
+  readonly analyzedImages$ = this._analyzedImages.asObservable();
+
+  constructor(private mediaStorageService: MediaStorageService) {
+    this.fetchAll();
+  }
+
+  get analyzedImages(): AnalyzedImageMetaData[] {
+    return this._analyzedImages.getValue();
+  }
+
+  set analyzedImages(val: AnalyzedImageMetaData[]) {
+    this._analyzedImages.next(val);
+  }
+
+
+  async fetchAll() {
+    await this.mediaStorageService.getAnalyzedImages().subscribe((res) => {
+      this.analyzedImages = res.images;
+    });
+  }
 }
