d337e1866cba7d2b612b416da1acefc8853b6d95
NJHKirsten <u19049014@tuks.co.za>
2021-08-16 12:55:57 +0200

    Revert "Revert "Added toolrunner thread interface""
    This reverts commit 7ca343b5

--- backend/HighFive/src/AnalysisTools/AnalysisThread/ToolRunner.cs
+++ backend/HighFive/src/AnalysisTools/AnalysisThread/ToolRunner.cs
@@ -11,7 +11,7 @@ using src.Subsystems.Analysis;
 
 namespace src.AnalysisTools.AnalysisThread
 {
-    public class ToolRunner
+    public class ToolRunner:IToolRunner
     {
         private BlockingCollection<byte[]> _frames = new();
         private readonly List<Tool> _tools;
