ccdd2adffb2948381c2c6a3cee9cdee999d2398f
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-08-16 21:18:29 +0200

    Store for analyzed videos created

--- web/src/app/services/analyzed-videos/analyzed-videos.service.ts
+++ web/src/app/services/analyzed-videos/analyzed-videos.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {MediaStorageService} from "../../apis/mediaStorage.service";
+import {AnalyzedVideoMetaData} from "../../models/analyzedVideoMetaData";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyzedVideosService {
 
-  constructor() { }
+
+  private readonly _analyzeVideos = new BehaviorSubject<AnalyzedVideoMetaData[]>([])
+  readonly analyzedVideos$ = this._analyzeVideos.asObservable();
+
+  constructor(private mediaStorageService: MediaStorageService) {
+    this.fetchAll();
+  }
+
+  get analyzeVideos(): AnalyzedVideoMetaData[] {
+    return this._analyzeVideos.getValue();
+  }
+
+  set analyzeVideos(val: AnalyzedVideoMetaData[]) {
+    this._analyzeVideos.next(val);
+  }
+
+  async fetchAll() {
+    await this.mediaStorageService.getAnalyzedVideos().subscribe((res) => {
+      this.analyzeVideos = res.videos;
+    });
+  }
 }
