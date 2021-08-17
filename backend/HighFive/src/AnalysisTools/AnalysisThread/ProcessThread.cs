39dff0b6654e3fe35c2695324ce632f7bda092de
NJHKirsten <u19049014@tuks.co.za>
2021-08-17 09:47:13 +0200

    Added Threads for analysis

---
+++ backend/HighFive/src/AnalysisTools/AnalysisThread/ProcessThread.cs
@@ -0,0 +1,47 @@
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace src.AnalysisTools.AnalysisThread
+{
+    public class ProcessThread
+    {
+        private BlockingCollection<byte[]> _frames = new();
+        
+        public ProcessThread()
+        {
+            var thread = new Thread(OnStart);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        
+        public void Enqueue(byte[] frame)
+        {
+            //Used by main thread to add new frames
+            _frames.Add(frame);
+        }
+
+        private void OnStart()
+        {
+            // foreach (var frame in _frames.GetConsumingEnumerable(CancellationToken.None))
+            // {
+            //     //ByteArray of length 1 indicates end of input
+            //     if (frame.Length == 1) break;
+            //     
+            //     //Analyse images
+            //     var outputs = _tools.Select(tool => tool.AnalyseFrame(frame)).ToList();
+            //     
+            //     //Draw Boxes
+            //     var image = BoxDrawer.DrawBoxes(frame, outputs);
+            //     byte[] outputFrame;
+            //     using (var ms = new MemoryStream())
+            //     {
+            //         image.Save(ms, _frameFormat);
+            //         outputFrame =  ms.ToArray();
+            //     }
+            //     
+            //     //Add to output queue
+            //     _outputQueue.Add(outputFrame);
+            // }
+        }
+    }
+}
--- backend/HighFive/src/AnalysisTools/AnalysisThread/ToolRunner.cs
+++ backend/HighFive/src/AnalysisTools/AnalysisThread/ToolRunner.cs
@@ -18,16 +18,29 @@ namespace src.AnalysisTools.AnalysisThread
         private readonly List<Tool> _tools;
         private BlockingCollection<byte[]> _outputQueue;
         private readonly ImageFormat _frameFormat = ImageFormat.Jpeg;//Format of output is changed here
- 
+        private List<BlockingCollection<AnalysisOutput>> _toolOutputs;
+        private List<ToolThreadAllocator> _toolThreadAllocators;
+
         public ToolRunner(List<Tool> tools, BlockingCollection<byte[]> outputQueue)
         {
             _outputQueue = outputQueue;
             _tools = tools;
+
+
             var thread = new Thread(OnStart);
             thread.IsBackground = true;
             thread.Start();
+
+            _toolOutputs = new List<BlockingCollection<AnalysisOutput>>();
+            _toolThreadAllocators = new List<ToolThreadAllocator>();
+            foreach (var tool in _tools)
+            {
+                _toolOutputs.Add(new BlockingCollection<AnalysisOutput>());
+                _toolThreadAllocators.Add(new ToolThreadAllocator(tool, _toolOutputs.Last()));
+            }
+
         }
- 
+
         public void Enqueue(byte[] frame)
         {
             //Used by main thread to add new frames
@@ -36,27 +49,19 @@ namespace src.AnalysisTools.AnalysisThread
  
         private void OnStart()
         {
+            
             //Loop keeps checking queue for new frames to analyse
             foreach (var frame in _frames.GetConsumingEnumerable(CancellationToken.None))
             {
-                //ByteArray of length 1 indicates end of input
-                if (frame.Length == 1) break;
-                
-                //Analyse images
-                var outputs = _tools.Select(tool => tool.AnalyseFrame(frame)).ToList();
-                
-                //Draw Boxes
-                var image = BoxDrawer.DrawBoxes(frame, outputs);
-                byte[] outputFrame;
-                using (var ms = new MemoryStream())
+                foreach (var toolAlloc in _toolThreadAllocators)
                 {
-                    image.Save(ms, _frameFormat);
-                    outputFrame =  ms.ToArray();
+                    toolAlloc.Enqueue(frame);
                 }
-                
-                //Add to output queue
-                _outputQueue.Add(outputFrame);
+                if (frame.Length == 1) break;
             }
+            
+            //TODO await all outputs
+            
         }
     }
 }
