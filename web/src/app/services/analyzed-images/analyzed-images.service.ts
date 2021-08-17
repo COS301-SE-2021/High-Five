5b0d941cad26735a1fed29e64fbfa792e2f2cc61
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-08-17 10:53:30 +0200

    Added analyse video and image functionality

--- web/src/app/services/analyzed-images/analyzed-images.service.ts
+++ web/src/app/services/analyzed-images/analyzed-images.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 import {MediaStorageService} from "../../apis/mediaStorage.service";
 import {AnalyzedImageMetaData} from "../../models/analyzedImageMetaData";
+import {AnalysisService} from "../../apis/analysis.service";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AnalyzedImagesService {
   private readonly _analyzedImages = new BehaviorSubject<AnalyzedImageMetaData[]>([])
   readonly analyzedImages$ = this._analyzedImages.asObservable();
 
-  constructor(private mediaStorageService: MediaStorageService) {
+  constructor(private mediaStorageService: MediaStorageService, private analysisService : AnalysisService) {
     this.fetchAll();
   }
 
@@ -23,7 +24,10 @@ export class AnalyzedImagesService {
     this._analyzedImages.next(val);
   }
 
-
+  async analyzeImage(mediaId: string, pipelineId : string , mediaType: string = 'image'){
+    await this.analysisService.analyzeMedia({mediaId: mediaId, pipelineId: pipelineId, mediaType: mediaType}).toPromise();
+    this.fetchAll();
+  }
   async fetchAll() {
     await this.mediaStorageService.getAnalyzedImages().subscribe((res) => {
       this.analyzedImages = res.images;
--- web/src/app/services/analyzed-videos/analyzed-videos.service.ts
+++ web/src/app/services/analyzed-videos/analyzed-videos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 import {MediaStorageService} from "../../apis/mediaStorage.service";
 import {AnalyzedVideoMetaData} from "../../models/analyzedVideoMetaData";
+import {AnalysisService} from "../../apis/analysis.service";
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class AnalyzedVideosService {
   private readonly _analyzeVideos = new BehaviorSubject<AnalyzedVideoMetaData[]>([])
   readonly analyzedVideos$ = this._analyzeVideos.asObservable();
 
-  constructor(private mediaStorageService: MediaStorageService) {
+  constructor(private mediaStorageService: MediaStorageService, private analysisService: AnalysisService) {
     this.fetchAll();
   }
 
@@ -22,6 +23,11 @@ export class AnalyzedVideosService {
 
   set analyzeVideos(val: AnalyzedVideoMetaData[]) {
     this._analyzeVideos.next(val);
+  }
+
+  async analyzeVideo(mediaId: string, pipelineId : string , mediaType: string = 'video'){
+    await this.analysisService.analyzeMedia({mediaId: mediaId, pipelineId: pipelineId, mediaType: mediaType}).toPromise();
+    this.fetchAll();
   }
 
   async fetchAll() {
