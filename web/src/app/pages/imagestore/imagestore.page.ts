0e33696d5a1bd097f98a49255b64eb28a6c41ee1
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-08-17 17:01:01 +0200

    Added filter in images media for analyzed/un-analyzed and all

--- web/src/app/pages/imagestore/imagestore.page.ts
+++ web/src/app/pages/imagestore/imagestore.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ImagesService} from "../../services/images/images.service";
 import {AnalyzedImagesService} from "../../services/analyzed-images/analyzed-images.service";
+import {MediaFilterComponent} from "../../components/media-filter/media-filter.component";
+import {PopoverController} from "@ionic/angular";
 
 @Component({
   selector: 'app-imagestore',
@@ -11,21 +13,39 @@ export class ImagestorePage implements OnInit {
   analyzedImageTrackFn = (ai, analyzed_image) => analyzed_image.id;
   imageTrackFn= (i, image) => image.id;
   segment : string;
-  constructor(public imagesService : ImagesService, public  analyzedImagesService : AnalyzedImagesService) {
+  constructor(public imagesService : ImagesService, public  analyzedImagesService : AnalyzedImagesService,
+              private popoverController : PopoverController) {
     this.segment='all'
   }
   ngOnInit() {
   }
-  /**
-   * This function will delete an image from the user's account, optimistic loading updates are used and in the event
-   * and error is thrown, the image is added back and an appropriate toast is shown
-   *
-   * @param image the data of the image we wish to upload
-   */
+
+  async displayFilterPopover(ev: any) {
+    const filterPopover = await this.popoverController.create({
+      component: MediaFilterComponent,
+      cssClass: 'media-filter',
+      animated: true,
+      translucent: true,
+      backdropDismiss: true,
+      event: ev,
+    });
+    await filterPopover.present();
+    await filterPopover.onDidDismiss().then(
+      data => {
+        if (data.data != undefined) {
+          if (data.data.segment != undefined) {
+            this.segment = data.data.segment;
+          }
+        }
+      }
+    );
+  }
 
   async uploadImage(image: any) {
 
     await this.imagesService.addImage(image.target.files[0]);
     //Nothing added here yet
   }
+
+
 }
