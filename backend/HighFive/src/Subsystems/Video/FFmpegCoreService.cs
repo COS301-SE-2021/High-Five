180f458c1abc9f255d6989dd6ae68b19d598d257
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-08-17 14:08:10 +0200

    Added comment for registerDecoder function

--- backend/HighFive/src/Subsystems/Video/FFmpegCoreService.cs
+++ backend/HighFive/src/Subsystems/Video/FFmpegCoreService.cs
@@ -10,14 +10,21 @@ namespace src.Subsystems.Video
         
         public FFmpegCoreService() { }
 
+        /*
+        *      Description:
+        *  This function registers an input and output stream with the FFmpegCore library.        
+        *  Input is read from the input stream and decoded into bitmap frames and piped back through the output stream.
+        *      Parameters:
+        * -> input : The input stream carrying the video
+        * -> output: The output stream carrying the bmp frames   
+        */
         public async Task registerDecoder(Stream input, Stream output)
         {
             {
                 await FFMpegArguments
                 .FromPipeInput(new StreamPipeSource(input))
                 .OutputToPipe(new StreamPipeSink(output), options => options
-                    .WithVideoCodec("bmp")
-                    .ForceFormat("webm"))
+                    .ForceFormat("bmp"))
                 .ProcessAsynchronously();
                 
             }
