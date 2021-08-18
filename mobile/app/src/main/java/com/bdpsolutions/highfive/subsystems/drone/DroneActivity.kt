b59eb3f9d3f8db34e3e9e5bb63b9d587fa121f21
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-08-18 14:17:24 +0200

    Removed redundant code from drone activity and added permission request for android version 6+

--- mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneActivity.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneActivity.kt
@@ -50,151 +50,14 @@ class DroneActivity : AppCompatActivity(), View.OnClickListener {
 
 
     private var btnLive: ToggleButton? = null
-    private var djiStreamer : DjiStreamer = DjiStreamer()
-    private val REQUEST_PERMISSION_CODE = 12345
-    private val REQUIRED_PERMISSION_LIST = arrayOf(
-        Manifest.permission.VIBRATE,
-        Manifest.permission.INTERNET,
-        Manifest.permission.ACCESS_WIFI_STATE,
-        Manifest.permission.WAKE_LOCK,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.ACCESS_NETWORK_STATE,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.CHANGE_WIFI_STATE,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.BLUETOOTH,
-        Manifest.permission.BLUETOOTH_ADMIN,
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.READ_PHONE_STATE
-    )
-
-    private val missingPermission: MutableList<String> = ArrayList()
-    private val isRegistrationInProgress = AtomicBoolean(false)
+    //private var djiStreamer : DjiStreamer = DjiStreamer()
 
     private fun showToast(toastMsg: String) {
         runOnUiThread { Toast.makeText(applicationContext, toastMsg, Toast.LENGTH_LONG).show() }
     }
-    private fun startSDKRegistration() {
-        if (isRegistrationInProgress.compareAndSet(false, true)) {
-            AsyncTask.execute {
-                showToast("registering, pls wait...")
-                DJISDKManager.getInstance().registerApp(applicationContext, object : SDKManagerCallback {
-                        override fun onRegister(djiError: DJIError) {
-                            if (djiError === DJISDKError.REGISTRATION_SUCCESS) {
-                                DJILog.e(
-                                    "App registration",
-                                    DJISDKError.REGISTRATION_SUCCESS.description
-                                )
-                                DJISDKManager.getInstance().startConnectionToProduct()
-                                showToast("Register Success")
-                            } else {
-                                showToast("Register sdk fails, check network is available")
-                            }
-                            Log.v(
-                                TAG,
-                                djiError.description
-                            )
-                        }
-
-                        override fun onProductDisconnect() {
-                            Log.d(
-                                TAG,
-                                "onProductDisconnect"
-                            )
-                            showToast("Product Disconnected")
-                        }
-
-                        override fun onProductConnect(baseProduct: BaseProduct) {
-                            Log.d(
-                                TAG,
-                                String.format("onProductConnect newProduct:%s", baseProduct)
-                            )
-                            showToast("Product Connected")
-                        }
-
-                        override fun onProductChanged(baseProduct: BaseProduct) {}
-                        override fun onComponentChange(
-                            componentKey: ComponentKey, oldComponent: BaseComponent,
-                            newComponent: BaseComponent
-                        ) {
-                            if (newComponent != null) {
-                                newComponent.setComponentListener { isConnected ->
-                                    Log.d(
-                                        TAG,
-                                        "onComponentConnectivityChanged: $isConnected"
-                                    )
-                                }
-                            }
-                            Log.d(
-                                TAG, String.format(
-                                    "onComponentChange key:%s, oldComponent:%s, newComponent:%s",
-                                    componentKey,
-                                    oldComponent,
-                                    newComponent
-                                )
-                            )
-                        }
-
-                        override fun onInitProcess(djisdkInitEvent: DJISDKInitEvent, i: Int) {}
-                        override fun onDatabaseDownloadProgress(l: Long, l1: Long) {}
-                    })
-            }
-        }
-    }
-
-    /**
-     * Checks if there is any missing permissions, and
-     * requests runtime permission if needed.
-     */
-    private fun checkAndRequestPermissions() {
-        // Check for permissions
-        for (eachPermission in REQUIRED_PERMISSION_LIST) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    eachPermission
-                ) != PackageManager.PERMISSION_GRANTED) {
-                missingPermission.add(eachPermission)
-            }
-        }
-        // Request for missing permissions
-        if (!missingPermission.isEmpty() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            ActivityCompat.requestPermissions(
-                this,
-                missingPermission.toTypedArray(),
-                REQUEST_PERMISSION_CODE
-            )
-        }
-    }
-
-    /**
-     * Result of runtime permission request
-     */
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String?>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        // Check for granted permission and remove from missing list
-        if (requestCode == REQUEST_PERMISSION_CODE) {
-            for (i in grantResults.indices.reversed()) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    missingPermission.remove(permissions[i])
-                }
-            }
-        }
-        // If there is enough permission, we will start the registration
-        if (missingPermission.isEmpty()) {
-            //startSDKRegistration()
-        } else {
-            showToast("Missing permissions!!!")
-        }
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        checkAndRequestPermissions()
         // When the compile and target version is higher than 22, please request the
         // following permissions at runtime to ensure the
         // SDK work well.
@@ -216,12 +79,35 @@ class DroneActivity : AppCompatActivity(), View.OnClickListener {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
             )
         }
+
+        // When the compile and target version is higher than 22, please request the
+        // following permissions at runtime to ensure the
+        // SDK work well.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.VIBRATE,
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.WAKE_LOCK,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.READ_PHONE_STATE
+                ), 1
+            )
+        }
     }
 
     override fun onClick(v: View?) = when (v!!.id) {
         binding.toggleLive.id -> {
             if(binding.toggleLive.isChecked){
-                djiStreamer.setupLiveStream()
+                //djiStreamer.setupLiveStream()
             }else{
                 //djiStreamer.stopStream()
             }
