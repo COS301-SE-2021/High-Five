82ee4b3ca5d425d6a163da7d36a98865446008d0
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-08-19 11:40:09 +0200

    logo using correct svg

--- web/src/test.ts
+++ web/src/test.ts
@@ -22,4 +22,5 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
-context.keys().map(context);
+//Auto generated file
+context.keys().map(context); //NOSONAR
