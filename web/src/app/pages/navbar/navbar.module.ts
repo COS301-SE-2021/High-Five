b333e33626ea7daf111f0ae0ffa6b77b0d8fdd73
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-08-24 10:12:13 +0200

    Fromatting code

--- web/src/app/pages/navbar/navbar.module.ts
+++ web/src/app/pages/navbar/navbar.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { NavbarPageRoutingModule } from './navbar-routing.module';
+import {NavbarPageRoutingModule} from './navbar-routing.module';
 
-import { NavbarPage } from './navbar.page';
+import {NavbarPage} from './navbar.page';
+import {ImagesService} from '../../services/images/images.service';
+import {VideosService} from '../../services/videos/videos.service';
+import {PipelineService} from '../../services/pipeline/pipeline.service';
+import {AnalyzedImagesService} from '../../services/analyzed-images/analyzed-images.service';
+import {AnalyzedVideosService} from '../../services/analyzed-videos/analyzed-videos.service';
+import {NotificationsService} from '../../services/notifications/notifications.service';
 
 @NgModule({
   imports: [
@@ -17,4 +23,9 @@ import { NavbarPage } from './navbar.page';
   ],
   declarations: [NavbarPage]
 })
-export class NavbarPageModule {}
+export class NavbarPageModule {
+  constructor(private imagesService: ImagesService, private videosService: VideosService,
+              private pipelineService: PipelineService, private analyzedImagesService: AnalyzedImagesService,
+              private analyzedVideosService: AnalyzedVideosService, private notificationsService: NotificationsService) {
+  }
+}
