245368c6afc5b94bf285abe5cfc011241b0819e0
Kyle Barry <u19232510@tuks.co.za>
2021-08-25 14:40:48 +0200

    Added uploader classes for uploading different media types  - Kyle Barry (u19232510)

--- mobile/app/src/main/java/com/bdpsolutions/highfive/services/mediaupload/uploader/MediaUploader.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/services/mediaupload/uploader/MediaUploader.kt
@@ -1,20 +1,23 @@
 package com.bdpsolutions.highfive.services.mediaupload.uploader
 
+import com.bdpsolutions.highfive.constants.MediaTypes
 import com.bdpsolutions.highfive.subsystems.image.model.ImageRepository
 import com.bdpsolutions.highfive.subsystems.video.model.repository.VideoDataRepository
 import com.bdpsolutions.highfive.utils.factories.RepositoryFactory
+import java.lang.Exception
 import javax.inject.Inject
 
 class MediaUploader {
 
     private var mediaType: String? = null
-    private var repository:
-
-    @Inject
-    lateinit var repoFactory: RepositoryFactory
+    private var uploader: IUploaderStrategy? = null
 
     fun setMediaType(type: String) : MediaUploader {
-        mediaType = type
+        uploader = when(type) {
+            MediaTypes.IMAGE -> ImageUploaderStrategy()
+            MediaTypes.VIDEO -> VideoUploaderStrategy()
+            else -> throw Exception("Unsupported media type: $type")
+        }
         return this
     }
 
