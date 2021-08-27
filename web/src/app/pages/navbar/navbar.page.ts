56602f1ac67d9b504072368bdc6b2b5f70c1818f
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-08-28 00:24:40 +0200

    Navbar link colouring fixed & routing

--- web/src/app/pages/navbar/navbar.page.ts
+++ web/src/app/pages/navbar/navbar.page.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ScreenSizeServiceService} from '../../services/screen-size-service.service';
 import {Router} from '@angular/router';
 import {MsalService} from '@azure/msal-angular';
@@ -12,20 +12,7 @@ import {AccountPopoverComponent} from '../../components/account-popover/account-
   styleUrls: ['./navbar.page.scss'],
 })
 export class NavbarPage implements OnInit {
-
-  @ViewChild('homeNav') homeNav: HTMLIonButtonElement;
-  @ViewChild('analyticsNav') analyticsNav: HTMLIonButtonElement;
-  @ViewChild('videoNav') videoNav: HTMLIonButtonElement;
-  @ViewChild('controlsNav') controlsNav: HTMLIonButtonElement;
-  //These links are arrays so that when the content is changed, it is shown in the HTML
-  homeLink = ['active-link'];
-  analyticsLink = ['link'];
-  mediaLink = ['link'];
-  controlsLink = ['link'];
-  liveLink = ['link'];
-
   isDesktop: boolean;
-  private navPages;
 
 
   constructor(private screenSizeService: ScreenSizeServiceService, private router: Router,
@@ -33,12 +20,7 @@ export class NavbarPage implements OnInit {
     this.screenSizeService.isDesktopView().subscribe(isDesktop => {
       this.isDesktop = isDesktop;
     });
-    this.navPages = {
-      homeNav: this.homeLink,
-      analyticsNav: this.analyticsLink,
-      videoNav: this.mediaLink,
-      controlsNav: this.controlsLink
-    };
+
   }
 
   ngOnInit() {
@@ -56,25 +38,6 @@ export class NavbarPage implements OnInit {
     });
   }
 
-  /**
-   * This function will navigate the application to the provided url, as well as change the styling of the
-   * navigation buttons to communicate to the user that they are at that url.
-   *
-   * @param url The url to navigate to
-   * @param tab The navigation button to set to be active
-   */
-  navigateTo(url: string, tab: string) {
-    // eslint-disable-next-line guard-for-in
-    for (const key in this.navPages) {
-      const value = this.navPages[key];
-      if (key !== tab) {
-        value[0] = 'link';
-      } else {
-        value[0] = 'active-link';
-      }
-    }
-    this.router.navigate([url]);
-  }
 
   /**
    * This function will display a popover containing the different media type pages' link to which the user can navigate
@@ -104,7 +67,7 @@ export class NavbarPage implements OnInit {
   async displayAccountPopover(ev: any) {
     const popoverComponent = await this.popoverController.create({
       component: AccountPopoverComponent,
-      cssClass:'accountOptionsPopover',
+      cssClass: 'accountOptionsPopover',
       animated: true,
       translucent: true,
       backdropDismiss: true,
