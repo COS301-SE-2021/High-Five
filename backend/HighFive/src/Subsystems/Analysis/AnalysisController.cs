4080dc889b807f2bb6089a35618a7ed195298c55
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-08-27 12:18:51 +0200

    Added authentication to Websocket controller.

--- backend/HighFive/src/Subsystems/Analysis/AnalysisController.cs
+++ backend/HighFive/src/Subsystems/Analysis/AnalysisController.cs
@@ -1,5 +1,4 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
