4bf66dfc724d0ab3fd9f1c33d6529a3a386ff566
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-01 14:10:38 +0200

    Refactored directories. Added InputMerger util class

---
+++ backend/HighFive/analysis_engine/Util/Pipes/InputMerger.cs
@@ -0,0 +1,21 @@
+﻿namespace analysis_engine.Util
+{
+    public class InputMerger : Pipe
+    {
+        private Pipe output;
+        public InputMerger(Pipe output)
+        {
+            this.output = output;
+        }
+
+        public void push(Data data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Data pop()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
