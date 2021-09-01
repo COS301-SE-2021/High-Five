9a72c98fb284ed4ecf8743a23c859ff42bd2ebef
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-01 13:50:13 +0200

    Implemented ConcurrentQueuePipe push method

--- backend/HighFive/analysis_engine/Util/ConcurrentQueuePipe.cs
+++ backend/HighFive/analysis_engine/Util/ConcurrentQueuePipe.cs
@@ -1,7 +1,24 @@
-﻿namespace analysis_engine.Util
+﻿using System.Collections.Concurrent;
+
+namespace analysis_engine.Util
 {
-    public class ConcurrentQueuePipe
+    public class ConcurrentQueuePipe : Pipe
     {
-        
+        private ConcurrentQueue<Data> dataQueue;
+
+        public ConcurrentQueuePipe()
+        {
+            dataQueue = new ConcurrentQueue<Data>();
+        }
+
+        public void push(Data data)
+        {
+            dataQueue.Enqueue(data);
+        }
+
+        public Data pop()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
