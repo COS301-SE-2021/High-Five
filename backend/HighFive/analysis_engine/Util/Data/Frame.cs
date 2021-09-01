1273e898307e5a05e425d4f531512f645b798e08
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-01 14:19:53 +0200

    Added Constructor and refreshFrame methods to Frame class

--- backend/HighFive/analysis_engine/Util/Data/Frame.cs
+++ backend/HighFive/analysis_engine/Util/Data/Frame.cs
@@ -2,6 +2,22 @@
 {
     public class Frame
     {
-        
+        private byte[] bitmap;
+        private int frameID;
+        public Frame(byte[] bitmap, int frameId)
+        {
+            refreshFrame(bitmap, frameId);
+        }
+
+        public Frame()
+        {
+        }
+
+        public void refreshFrame(byte[] bitmap, int frameId)
+        {
+            this.bitmap = bitmap;
+            frameID = frameId;
+        }
+
     }
 }
