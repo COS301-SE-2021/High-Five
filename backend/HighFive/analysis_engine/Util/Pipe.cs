17c8c26b8296aef8d87260fcc5a0f349738f53ab
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-01 13:45:01 +0200

    Added abstract methods with comments to Pipe interface

--- backend/HighFive/analysis_engine/Util/Pipe.cs
+++ backend/HighFive/analysis_engine/Util/Pipe.cs
@@ -2,7 +2,23 @@
 
 namespace analysis_engine.Util
 {
-    public abstract class Pipe
+    public interface Pipe
     {
+        
+        /*
+         *      Description:
+         * This is an abstract function which should be implemented to add a Data object to the Pipe.
+         *      Parameters:
+         * -> data: The Data to be added to the Pipe.
+         */
+        public abstract void push(Data data);
+        
+        /*
+         *      Description:
+         * This is an abstract function which should be implemented to add a Data object to the Pipe.
+         *      Parameters:
+         * -> data: The Data to be added to the Pipe.
+         */
+        public abstract Data pop();
     }
 }
