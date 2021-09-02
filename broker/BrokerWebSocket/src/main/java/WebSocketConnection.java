6b26ee09b9f11b782fa54b4ea55689bd9b4e5428
Kyle Barry <u19232510@tuks.co.za>
2021-09-02 15:25:14 +0200

    Finalised WebSocketConnection - Kyle Barry (u19232510)

--- broker/BrokerWebSocket/src/main/java/WebSocketConnection.java
+++ broker/BrokerWebSocket/src/main/java/WebSocketConnection.java
@@ -4,46 +4,47 @@ import java.io.*;
 import java.net.Socket;
 import org.apache.commons.io.IOUtil;
 
-@ServerEndpoint(
-        value="/socket"
-)
+/**
+ * WebSocket class that is loaded into an embedded Tomcat server. This class acts as a middleman
+ * between the web client and the Broker, forwarding requests and responses between them.
+ */
+@ServerEndpoint(value="/broker")
 public class WebSocketConnection {
 
-    private Session session;
 
     @OnOpen
-    public void onOpen(Session session) throws IOException {
-        this.session = session;
-        System.out.println("Connection opened");
-        // Get session and WebSocket connection
-    }
+    public void onOpen(Session session) throws IOException {}
 
+    /**
+     * Fetches a request from the client and passes the request to the Broker.
+     * Reads a response from the Broker and passes the response to the client.
+     *
+     * @param session WebSocket session to fetch and send data
+     * @param message THe message from the web client
+     */
     @OnMessage
     public void onMessage(Session session, String message) throws IOException {
+
+        //Send request to broker
         Socket brokerConnection = new Socket("localhost", 6666);
         Writer serverInfoRequest = new BufferedWriter(new OutputStreamWriter(
                 brokerConnection.getOutputStream()));
 
         serverInfoRequest.append(message).flush();
 
+        //Read response from broker
         StringWriter infoDataWriter = new StringWriter();
         IOUtil.copy(brokerConnection.getInputStream(), infoDataWriter, "UTF-8");
         String infoData = infoDataWriter.toString();
         brokerConnection.close();
 
+        //Send response to web client
         session.getBasicRemote().sendText(infoData);
-
     }
 
     @OnClose
-    public void onClose(Session session) throws IOException {
-        // WebSocket connection closes
-        System.out.println("Connection closed");
-    }
+    public void onClose(Session session) throws IOException {}
 
     @OnError
-    public void onError(Session session, Throwable throwable) {
-        // Do error handling here
-    }
-
+    public void onError(Session session, Throwable throwable) {}
 }
