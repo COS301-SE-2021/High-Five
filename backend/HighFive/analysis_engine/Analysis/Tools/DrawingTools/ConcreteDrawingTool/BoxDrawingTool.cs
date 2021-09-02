11dd71404d5dcaa971beae869a1f4f2c96cb8142
NJHKirsten <u19049014@tuks.co.za>
2021-09-02 13:33:00 +0200

    Finished Drawing tool

--- backend/HighFive/analysis_engine/Analysis/Tools/DrawingTools/ConcreteDrawingTool/BoxDrawingTool.cs
+++ backend/HighFive/analysis_engine/Analysis/Tools/DrawingTools/ConcreteDrawingTool/BoxDrawingTool.cs
@@ -1,7 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using analysis_engine.Analysis.Util.Data;
+using analysis_engine.Analysis.Util.Data.ConcreteData;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
 namespace analysis_engine.Analysis.Tools.DrawingTools.ConcreteDrawingTool
 {
     public class BoxDrawingTool : DrawingTool
     {
-        
+        public Image<Rgb, byte> Draw(Image<Rgb, byte> image, Data data)
+        {
+            var count = 1;
+            var outputs = data.Meta;
+            foreach (var output in outputs.Cast<BoxCoordinateData>())
+            {
+                for (var i = 0; i < output.Classes.Count; i++)
+                {
+                    var box = new Rectangle(Convert.ToInt32(output.Boxes[i * 4]),
+                        Convert.ToInt32(output.Boxes[i * 4 + 1]), 
+                        Convert.ToInt32(output.Boxes[i * 4 + 2]),
+                        Convert.ToInt32(output.Boxes[i * 4 + 3]));
+                    var point = new Point(Convert.ToInt32(output.Boxes[i * 4]),
+                        Convert.ToInt32(output.Boxes[i * 4 + 1] - 10.0 * image.Height / 2286.0));
+                    CvInvoke.Rectangle(image, box, new Bgr(Color.Red).MCvScalar, 5, LineType.Filled);
+                    CvInvoke.PutText(image, output.Classes[i].ToUpper(), point, FontFace.HersheyTriplex, 2.0, new Bgr(Color.Red).MCvScalar, 5);
+                }
+
+                var textPoint = new Point(image.Width / 445, count*6*image.Height / 229);
+                CvInvoke.PutText(image, "Vehicle Count: "+output.Classes.Count, textPoint, FontFace.HersheyTriplex, 2.0, new Bgr(Color.Red).MCvScalar, 5);
+                count++;
+            }
+            return image;
+        }
     }
 }
--- backend/HighFive/analysis_engine/Analysis/Tools/DrawingTools/DrawingTool.cs
+++ backend/HighFive/analysis_engine/Analysis/Tools/DrawingTools/DrawingTool.cs
@@ -1,7 +1,11 @@
+using analysis_engine.Analysis.Util.Data;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
 namespace analysis_engine.Analysis.Tools.DrawingTools
 {
     public interface DrawingTool
     {
-        
+        public Image<Rgb, byte> Draw(Image<Rgb, byte> image, Data data);
     }
 }
