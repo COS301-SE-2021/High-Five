820bc4e93ea990607a777c179a72c4ace2adfb0c
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-09-02 10:53:08 +0200

    Implemented UploadAnalysisTool service contract.

--- backend/HighFive/src/Subsystems/Tools/IToolService.cs
+++ backend/HighFive/src/Subsystems/Tools/IToolService.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Org.OpenAPITools.Models;
 
 namespace src.Subsystems.Tools
 {
     public interface IToolService
     {
-        public void UploadAnalysisTool(IFormFile sourceCode, IFormFile model, string toolName);
+        public Task<bool> UploadAnalysisTool(IFormFile sourceCode, IFormFile model, string toolName);
         public void UploadDrawingTool(IFormFile sourceCode, string toolName);
         public void DeleteTool(DeleteToolRequest request);
         public void GetAllTools();
--- backend/HighFive/src/Subsystems/Tools/ToolService.cs
+++ backend/HighFive/src/Subsystems/Tools/ToolService.cs
@@ -1,13 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
+using Accord.Math;
+using Microsoft.AspNetCore.Http;
 using Org.OpenAPITools.Models;
+using src.Storage;
 
 namespace src.Subsystems.Tools
 {
     public class ToolService: IToolService
     {
-        public void UploadAnalysisTool(IFormFile sourceCode, IFormFile model, string toolName)
+        private readonly IStorageManager _storageManager;
+        private const string ContainerName = "tools";
+
+        public ToolService(IStorageManager storageManager)
         {
-            throw new System.NotImplementedException();
+            _storageManager = storageManager;
+        }
+        
+        public async Task<bool> UploadAnalysisTool(IFormFile sourceCode, IFormFile model, string toolName)
+        {
+            var generatedToolName = _storageManager.HashMd5(toolName);
+            if (ToolExists(generatedToolName))
+            {
+                return false;
+            }
+
+            var sourceCodeName = _storageManager.HashMd5(sourceCode.FileName);
+            var sourceCodeFile = _storageManager.CreateNewFile(sourceCodeName + ".cs", ContainerName+ "/analysis/" + generatedToolName).Result;
+            sourceCodeFile.AddMetadata("toolName",toolName);
+            await sourceCodeFile.UploadFile(sourceCode);
+
+            var modelNameArr = model.FileName.Split(".");
+            var modelName = _storageManager.HashMd5(model.FileName);
+            var modelFile = _storageManager
+                .CreateNewFile(modelName + "." + modelNameArr[^1], ContainerName + "/analysis/" + generatedToolName).Result;
+            await modelFile.UploadFile(model);
+
+            return true;
         }
 
         public void UploadDrawingTool(IFormFile sourceCode, string toolName)
@@ -23,6 +51,13 @@ namespace src.Subsystems.Tools
         public void GetAllTools()
         {
             throw new System.NotImplementedException();
+        }
+
+        private bool ToolExists(string toolName)
+        {
+            var toolsFile = _storageManager.GetFile("tools.txt", "").Result;
+            var toolsArray = toolsFile.ToText().Result.Split("\n");
+            return toolsArray.IndexOf(toolName) != -1;
         }
     }
 }
