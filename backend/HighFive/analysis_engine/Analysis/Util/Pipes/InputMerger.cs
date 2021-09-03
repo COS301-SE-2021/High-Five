fde491d100cc1e49a62e2a57938c24adb176ceac
NJHKirsten <u19049014@tuks.co.za>
2021-09-03 16:10:33 +0200

    Updated InputMerger and Tool inheritance

--- backend/HighFive/analysis_engine/Analysis/Util/Pipes/InputMerger.cs
+++ backend/HighFive/analysis_engine/Analysis/Util/Pipes/InputMerger.cs
@@ -1,27 +1,40 @@
-﻿using System.Linq;
+﻿using System.Collections.Concurrent;
+using System.Linq;
 using analysis_engine.Util;
 
 namespace analysis_engine.Analysis.Util.Pipes
 {
     public class InputMerger : Pipe
     {
-        private Pipe _output;
-        private Data.Data _finalData;
-        public InputMerger(Pipe output)
+        private readonly Pipe _output;
+        private ConcurrentDictionary<int, Data.Data> _finalData;
+        private int _numPipelines;
+        private ConcurrentDictionary<int, int> _piplineCountdowns;
+        public InputMerger(Pipe output, int numPipelines)
         {
             this._output = output;
-            _finalData = null;
+            this._numPipelines = numPipelines;
+            _finalData = new ConcurrentDictionary<int, Data.Data>();
+            _piplineCountdowns = new ConcurrentDictionary<int, int>();
         }
 
         public void push(Data.Data data)
         {
-            if (_finalData == null)
+            if (_finalData.TryAdd(data.Frame.FrameID, data))
             {
-                _finalData = data;
+                _piplineCountdowns.TryAdd(data.Frame.FrameID, _numPipelines-1);
             }
             else
             {
-                _finalData.Meta=_finalData.Meta.Concat(data.Meta).ToList();
+                _finalData[data.Frame.FrameID].Meta = _finalData[data.Frame.FrameID].Meta.Concat(data.Meta).ToList();
+                _piplineCountdowns[data.Frame.FrameID]--;
+            }
+
+            if (_piplineCountdowns[data.Frame.FrameID] == 0)
+            {
+                Data.Data output;
+                _finalData.TryRemove(data.Frame.FrameID, out output);
+                _output.push(output);
             }
         }
 
