b22a5ba09fd8afa89b4bb5fd4af070207e05169a
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-03 17:48:40 +0200

    Started implementing ConcurrentInputMergerPipe

--- backend/HighFive/analysis_engine/Analysis/Util/Pipes/ConcurrentInputMergerPipe.cs
+++ backend/HighFive/analysis_engine/Analysis/Util/Pipes/ConcurrentInputMergerPipe.cs
@@ -1,7 +1,17 @@
-﻿namespace analysis_engine.Analysis.Util.Pipes
+﻿using analysis_engine.Util;
+
+namespace analysis_engine.Analysis.Util.Pipes
 {
-    public class ConcurrentInputMergerPipe
+    public class ConcurrentInputMergerPipe : Pipe
     {
-        
+        public void push(Data.Data data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Data.Data pop()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
