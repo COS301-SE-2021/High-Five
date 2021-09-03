2955a97292e8f1c41283bf50e0bf3bde83ee8e05
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-03 16:26:43 +0200

    Implemented AnalysisToolBuilder BuildTool and getTool

--- backend/HighFive/analysis_engine/Analysis/Tools/Builders/AnalysisToolBuilder.cs
+++ backend/HighFive/analysis_engine/Analysis/Tools/Builders/AnalysisToolBuilder.cs
@@ -1,4 +1,5 @@
-﻿using analysis_engine.Tools;
+﻿using analysis_engine.Analysis.Tools.ConcreteTools;
+using analysis_engine.Tools;
 
 namespace analysis_engine.Analysis.Tools.Builders
 {
@@ -6,12 +7,32 @@ namespace analysis_engine.Analysis.Tools.Builders
     {
         public override void BuildTool(string name)
         {
-            throw new System.NotImplementedException();
+            switch (name)
+            {
+                case "people-0":
+                    Tool = new PersonRecognitionTool();
+                    break;
+                case "people-1":
+                    Tool = new SelfDrawingPersonRecognitionTool();
+                    break;
+                case "animals-0":
+                    Tool = new AnimalRecognitionTool();
+                    break;
+                case "animals-1":
+                    Tool = new SelfDrawingAnimalRecognitionTool();
+                    break;
+                case "vehicles-0":
+                    Tool = new VehicleRecognitionTool();
+                    break;
+                case "vehicles-1":
+                    Tool = new SelfDrawingAnimalRecognitionTool();
+                    break;
+            }
         }
 
         public override Tool GetTool()
         {
-            throw new System.NotImplementedException();
+            return Tool;
         }
     }
 }
