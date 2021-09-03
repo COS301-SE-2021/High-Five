cb033684cd5fb76c6a7d4a2cfa2afa279ce3b0d0
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-03 19:08:12 +0200

    WIP created PipelineBuilder class

--- backend/HighFive/analysis_engine/Analysis/Pipeline/PipelineBuilder/PipelineBuilder.cs
+++ backend/HighFive/analysis_engine/Analysis/Pipeline/PipelineBuilder/PipelineBuilder.cs
@@ -1,7 +1,17 @@
-﻿namespace analysis_engine.Analysis.Pipeline.PipelineBuilder
+﻿using analysis_engine.Util;
+
+namespace analysis_engine.Analysis.Pipeline.PipelineBuilder
 {
-    public class PipelineBuilder
+    public abstract class PipelineBuilder
     {
-        
+        private Pipeline _pipeline;
+
+        public PipelineBuilder()
+        {
+        }
+
+        public abstract void BuildPipeline();
+        public abstract void AddSource(Pipe source);
+        public abstract void AddDrain(Pipe drain);
     }
 }
