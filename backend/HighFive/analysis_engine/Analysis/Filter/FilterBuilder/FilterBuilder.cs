392296b43395c3a82dc968e8a97c3786ae79bb36
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-03 18:12:56 +0200

    WIP working on filterbuilder

--- backend/HighFive/analysis_engine/Analysis/Filter/FilterBuilder/FilterBuilder.cs
+++ backend/HighFive/analysis_engine/Analysis/Filter/FilterBuilder/FilterBuilder.cs
@@ -1,7 +1,30 @@
-﻿namespace analysis_engine.Filter.FilterBuilder
+﻿using analysis_engine.Util;
+
+namespace analysis_engine.Filter.FilterBuilder
 {
     public class FilterBuilder
     {
-        
+        private Filter _filter;
+
+        public Filter GetFilter()
+        {
+            return _filter;
+        }
+
+        public void AddToolContainer(ToolContainer toolContainer)
+        {
+        }
+
+        public void AddInput(Pipe input)
+        {
+        }
+
+        public void AddOutput(Pipe output)
+        {
+        }
+
+        public void BuildFilter()
+        {
+        }
     }
 }
