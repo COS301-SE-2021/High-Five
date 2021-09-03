03b65c5c78c06adf7a355dd00986f4347a931448
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-03 15:59:48 +0200

    Work in progress implementing ToolBuilders

--- backend/HighFive/analysis_engine/Analysis/Tools/Builders/AnalysisToolBuilder.cs
+++ backend/HighFive/analysis_engine/Analysis/Tools/Builders/AnalysisToolBuilder.cs
@@ -1,7 +1,15 @@
 ﻿namespace analysis_engine.Tools.Builders
 {
-    public class AnalysisToolBuilder
+    public class AnalysisToolBuilder : ToolBuilder
     {
-        
+        public override void buildTool(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Tool getTool()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- backend/HighFive/analysis_engine/Analysis/Tools/Builders/ToolBuilder.cs
+++ backend/HighFive/analysis_engine/Analysis/Tools/Builders/ToolBuilder.cs
@@ -1,8 +1,10 @@
-﻿namespace analysis_engine.Tools.Builders
+﻿using System;
+
+namespace analysis_engine.Tools.Builders
 {
     public abstract class ToolBuilder
     {
-        public abstract void buildTool();
+        public abstract void buildTool(String name);
         public abstract Tool getTool();
     }
 }
