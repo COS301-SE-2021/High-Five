f836a4e5f59fc96a99bda1b099c7a5540b7035b3
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-04 22:46:06 +0200

    Added Start() function to ConcurrentInputMergerPipe class

--- backend/HighFive/analysis_engine/Analysis/Util/Pipes/ConcurrentInputMergerPipe.cs
+++ backend/HighFive/analysis_engine/Analysis/Util/Pipes/ConcurrentInputMergerPipe.cs
@@ -1,13 +1,16 @@
-﻿using analysis_engine.Util;
+﻿using System.Threading.Tasks;
+using analysis_engine.Analysis.Util.Data;
+using analysis_engine.Util;
 
 namespace analysis_engine.Analysis.Util.Pipes
 {
     public class ConcurrentInputMergerPipe : Pipe
     {
         private Pipe _output;
-
+        private Buffer buffer;
         public ConcurrentInputMergerPipe()
         {
+            buffer = new RingBuffer(5000);
         }
 
         public void SetOutput(Pipe output)
@@ -15,9 +18,20 @@ namespace analysis_engine.Analysis.Util.Pipes
             _output = output;
         }
 
+        public void Start()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    _output.Push(buffer.Pop());
+                }
+            });
+        }
+
         public void Push(Data.Data data)
         {
-            throw new System.NotImplementedException();
+            buffer.Push(data);
         }
 
         public Data.Data Pop()
