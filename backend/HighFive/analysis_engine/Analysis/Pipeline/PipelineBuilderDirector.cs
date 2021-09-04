dadb4571c082efed9fa7b638e8a720c8d841ace2
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-04 16:22:32 +0200

    WIP PipelineBuilderDirector implementation

--- backend/HighFive/analysis_engine/Analysis/Pipeline/PipelineBuilderDirector.cs
+++ backend/HighFive/analysis_engine/Analysis/Pipeline/PipelineBuilderDirector.cs
@@ -1,7 +1,25 @@
-﻿namespace analysis_engine.Analysis.Pipeline
+﻿using System;
+using analysis_engine.Analysis.Tools.Builders;
+using analysis_engine.Filter.FilterBuilder;
+using analysis_engine.Filter.ToolContainerBuilder;
+using analysis_engine.Util.Factories;
+;
+namespace analysis_engine.Analysis.Pipeline
 {
     public class PipelineBuilderDirector
     {
-        
+        private PipeFactory[] _pipeFactories;
+        private ToolBuilder[] _toolBuilders;
+        private ToolContainerBuilder[] _toolContainerBuilders;
+        private FilterBuilder _filterBuilder;
+        private PipelineBuilder.PipelineBuilder _pipelineBuilder;
+
+        public PipelineBuilderDirector(PipelineBuilder.PipelineBuilder pipelineBuilder)
+        {
+        }
+
+        public void Construct(String pipeline)
+        {
+        }
     }
 }
