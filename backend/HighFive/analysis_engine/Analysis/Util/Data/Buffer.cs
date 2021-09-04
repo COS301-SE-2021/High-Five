6d4e102f3d8e61cd8f7072ef5e97d227f4cd0505
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-04 21:11:29 +0200

    Implemented Buffer abstract class

--- backend/HighFive/analysis_engine/Analysis/Util/Data/Buffer.cs
+++ backend/HighFive/analysis_engine/Analysis/Util/Data/Buffer.cs
@@ -1,7 +1,20 @@
-﻿namespace analysis_engine.Util
+﻿using analysis_engine.Analysis.Util.Data;
+
+namespace analysis_engine.Util
 {
-    public class Buffer
+    public abstract class Buffer
     {
+        private long _size;
+        public long Head { get; set; }
+
+        protected Buffer(long size)
+        {
+            _size = size;
+            Head = 0;
+        }
+
+        public abstract void Push(Data data);
+        public abstract Data Pop();
         
     }
 }
