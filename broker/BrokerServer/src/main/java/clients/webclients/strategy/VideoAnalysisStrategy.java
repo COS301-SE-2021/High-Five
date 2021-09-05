1936d04c3f1756ed96f382902b012005023d78fc
Kyle Barry <u19232510@tuks.co.za>
2021-09-05 16:15:00 +0200

    Added code for sending command to analyse media. - Kyle Barry (u19232510)

--- broker/BrokerServer/src/main/java/clients/webclients/strategy/VideoAnalysisStrategy.java
+++ broker/BrokerServer/src/main/java/clients/webclients/strategy/VideoAnalysisStrategy.java
@@ -3,7 +3,7 @@ package clients.webclients.strategy;
 import dataclasses.serverinfo.ServerInformation;
 import dataclasses.clientrequest.AnalysisRequest;
 
-import java.io.Writer;
+import java.io.*;
 
 public class VideoAnalysisStrategy implements AnalysisStrategy{
 
@@ -13,7 +13,14 @@ public class VideoAnalysisStrategy implements AnalysisStrategy{
      * @param information
      */
     @Override
-    public void processRequest(AnalysisRequest request, ServerInformation information, Writer writer) {
+    public void processRequest(AnalysisRequest request, ServerInformation information, Writer writer) throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        Process proc = rt.exec(System.getenv("KAFKA_SEND_COMMAND"));
 
+
+        String line = null;
+
+        BufferedWriter outputStreamWriter =
+                new BufferedWriter(new OutputStreamWriter(proc.getOutputStream()));
     }
 }
