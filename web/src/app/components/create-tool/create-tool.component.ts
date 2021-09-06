012319ba6121a21aaa43dfe8be62286e92b13ce3
Jeanré du Plessis <jeanreduplessis2000@gmail.com>
2021-09-06 15:54:14 +0200

    Created component for tool creation

---
+++ web/src/app/components/create-tool/create-tool.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-tool',
+  templateUrl: './create-tool.component.html',
+  styleUrls: ['./create-tool.component.scss'],
+})
+export class CreateToolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
