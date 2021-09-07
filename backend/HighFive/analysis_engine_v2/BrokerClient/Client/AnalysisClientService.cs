cff91ac691e87fd6b3ce527c81643eba53500248
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-09-07 15:30:29 +0200

    Moved Broker Analysis Client code into analysis_engine_v2 project.

---
+++ backend/HighFive/analysis_engine_v2/BrokerClient/Client/AnalysisClientService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using broker_analysis_client.Client.Models;
+using broker_analysis_client.Storage;
+
+namespace broker_analysis_client.Client
+{
+    public class AnalysisClientService: IAnalysisClientService
+    {
+        private readonly IStorageManager _storageManager;
+
+        public AnalysisClientService()
+        {
+            _storageManager = new StorageManager();
+        }
+        
+        public AnalyzedImageMetaData StoreImage(byte[] image, AnalyzeImageRequest request)
+        {
+            //var blobFile = _storageManager.CreateNewFile(fileName, "analyzed/video").Result;
+            throw new NotImplementedException();
+        }
+
+        public AnalyzedVideoMetaData StoreVideo(byte[] video, AnalyzeVideoRequest requests)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<byte[]> GetVideo(string videoId)
+        {
+            var video = _storageManager.GetFile(videoId + ".mp4", "video").Result;
+            if (!await video.Exists())
+            {
+                return null;
+            }
+
+            return await video.ToByteArray();
+        }
+
+        public async Task<byte[]> GetImage(string imageId)
+        {
+            var image = _storageManager.GetFile(imageId + ".mp4", "image").Result;
+            if (!await image.Exists())
+            {
+                return null;
+            }
+
+            return await image.ToByteArray();
+        }
+
+        public AnalysisTool GetAnalysisTool(string toolId)
+        {
+            var toolSet = _storageManager.GetAllFilesInContainer("tools/analysis/" + toolId).Result;
+            IBlobFile sourceCodeFile = null;
+            IBlobFile modelFile = null;
+            foreach (var tool in toolSet)
+            {
+                if (tool.GetMetaData("toolName") != null)
+                {
+                    sourceCodeFile = tool;
+                }
+                else
+                {
+                    modelFile = tool;
+                }
+            }
+
+            var response = new AnalysisTool
+            {
+                ModelPath = Path.GetTempFileName()
+            };
+
+            var model = File.Create(response.ModelPath);
+            using var ms = modelFile.ToStream().Result;
+            var bytes = new byte[ms.Length];
+            ms.Read(bytes, 0, (int) ms.Length);
+            model.Write(bytes, 0, bytes.Length);
+            ms.Close();
+
+            response.SourceCode = sourceCodeFile?.ToText().Result;
+            
+            return response;
+        }
+
+        public string GetDrawingTool(string toolId)
+        {
+            var toolSet = _storageManager.GetAllFilesInContainer("tools/drawing/" + toolId).Result;
+            var drawingToolFile = toolSet[0];
+            return drawingToolFile.ToText().Result;
+        }
+
+        public void UnloadAnalysisModel(string modelPath)
+        {
+            File.Delete(modelPath);
+        }
+    }
+}
---
+++ backend/HighFive/analysis_engine_v2/BrokerClient/Storage/IStorageManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace broker_analysis_client.Storage
+{
+    public interface IStorageManager
+    {
+        public Task<IBlobFile> GetFile(string fileName, string container, bool create=false);
+        public Task<List<IBlobFile>> GetAllFilesInContainer(string container);
+        public Task<IBlobFile> CreateNewFile(string name, string container);
+        public string RandomString(int length=5);
+        public string HashMd5(string source);
+        public Task<bool> SetBaseContainer(string container);
+        public bool IsContainerSet();
+        public string GetCurrentContainer();
+        public Task DeleteAllFilesInContainer(string container);
+    }
+}
