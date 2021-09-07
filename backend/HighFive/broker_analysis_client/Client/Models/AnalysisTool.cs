4b2052db8d8073aab6e5c33eca94a2c524d210bb
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-09-07 15:31:14 +0200

    Removed broker_analysis_client as a project.

--- backend/HighFive/broker_analysis_client/Client/Models/AnalysisTool.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace broker_analysis_client.Client.Models
-{
-    public class AnalysisTool
-    {
-        public string ModelPath { get; set; }
-        public string SourceCode { get; set; }
-    }
-}
