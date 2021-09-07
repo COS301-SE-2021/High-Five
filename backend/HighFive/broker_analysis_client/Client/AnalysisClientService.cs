2e7c0769a905e67837f0af6c9adccec99b550a80
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-09-07 11:02:30 +0200

    Added concrete implementation skeleton for AnalysisClientService.cs

---
+++ backend/HighFive/broker_analysis_client/Client/AnalysisClientService.cs
@@ -0,0 +1,50 @@
+﻿using broker_analysis_client.Client.Models;
+using broker_analysis_client.Storage;
+
+namespace broker_analysis_client.Client
+{
+    public class AnalysisClientService: IAnalysisClientService
+    {
+        private readonly IStorageManager _storageManager;
+
+        public AnalysisClientService()
+        {
+            _storageManager = new StorageManager();
+        }
+        
+        public AnalyzedImageMetaData StoreImage(byte[] image)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AnalyzedVideoMetaData StoreVideo(byte[] video)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public byte[] GetVideo(string videoId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public byte[] GetImage(string imageId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetAnalysisTool(string toolId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetDrawingTool(string toolId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UnloadAnalysisModel(string modelId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- backend/HighFive/broker_analysis_client/Client/IAnalysisClientService.cs
+++ backend/HighFive/broker_analysis_client/Client/IAnalysisClientService.cs
@@ -2,14 +2,17 @@
 
 namespace broker_analysis_client.Client
 {
-    public interface IAnalysisClient
+    public interface IAnalysisClientService
     {
         public AnalyzedImageMetaData StoreImage(byte[] image);
         public AnalyzedVideoMetaData StoreVideo(byte[] video);
         public byte[] GetVideo(string videoId);
         public byte[] GetImage(string imageId);
 
-        public string GetAnalysisTool();
-        public string GetDrawingTool();
+        public string GetAnalysisTool(string toolId);
+        public string GetDrawingTool(string toolId);
+        public void UnloadAnalysisModel(string modelId);
+        
+        //TODO: GetPipeline functionality
     }
 }
