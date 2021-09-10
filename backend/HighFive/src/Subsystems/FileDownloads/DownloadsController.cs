0d4c5e454bdc857a62a62861280b507540cb195e
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-09-10 09:10:20 +0200

    Pushed DownloadsController.

---
+++ backend/HighFive/src/Subsystems/FileDownloads/DownloadsController.cs
@@ -0,0 +1,41 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Org.OpenAPITools.Controllers;
+
+namespace src.Subsystems.FileDownloads
+{
+    public class DownloadsController: DownloadsApiController
+    {
+        private readonly IDownloadsService _downloadsService;
+        private bool _baseContainerSet;
+
+        public DownloadsController(IDownloadsService downloadsService)
+        {
+            _downloadsService = downloadsService;
+            _baseContainerSet = false;
+        }
+        
+        public override IActionResult DownloadApk()
+        {
+            return StatusCode(503, null);
+            /*var response = _downloadsService.DownloadApk();
+            return StatusCode(200, response);*/
+        }
+
+        public override IActionResult DownloadSdkFiles()
+        {
+            return StatusCode(503, null);
+            /*var response = _downloadsService.DownloadSdkFiles();
+            return StatusCode(200, response);*/
+        }
+
+        public override IActionResult DownloadSdkManual()
+        {
+            return StatusCode(503, null);
+            /*var response = _downloadsService.DownloadSdkManual();
+            return StatusCode(200, response);*/
+        }
+    }
+}
