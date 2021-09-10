2a5fd5ee9e41e284b4910390d7b29e50ce415857
NJHKirsten <u19049014@tuks.co.za>
2021-09-10 11:59:23 +0200

    Implemented FrameGrabbers

--- backend/HighFive/analysis_engine_v2/Video/ConcreteFrameGrabber/ImageFrameGrabber.cs
+++ backend/HighFive/analysis_engine_v2/Video/ConcreteFrameGrabber/ImageFrameGrabber.cs
@@ -1,7 +1,18 @@
+using Emgu.CV;
+using Emgu.CV.Structure;
+
 namespace analysis_engine.Video
 {
-    public class ImageFrameGrabber
+    public class ImageFrameGrabber : FrameGrabber
     {
-        
+        public override void Init(string url)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Image<Rgb, byte> GetNextFrame()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- backend/HighFive/analysis_engine_v2/Video/ConcreteFrameGrabber/VideoFrameGrabber.cs
+++ backend/HighFive/analysis_engine_v2/Video/ConcreteFrameGrabber/VideoFrameGrabber.cs
@@ -1,7 +1,18 @@
+using Emgu.CV;
+using Emgu.CV.Structure;
+
 namespace analysis_engine.Video
 {
-    public class VideoFrameGrabber
+    public class VideoFrameGrabber : FrameGrabber
     {
-        
+        public override void Init(string url)
+        {
+            Capture = new VideoCapture(url);
+        }
+
+        public override Image<Rgb, byte> GetNextFrame()
+        {
+            return Capture.QueryFrame().ToImage<Rgb, byte>();
+        }
     }
 }
