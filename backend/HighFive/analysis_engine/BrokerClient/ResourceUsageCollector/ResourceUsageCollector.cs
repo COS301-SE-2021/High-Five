1cb62ec3743bbe92400aca73721361eeaf10b3b7
Kyle Barry <u19232510@tuks.co.za>
2021-09-10 17:23:55 +0200

    Server information is collected and sent to Kafka - Kyle Barry (u19232510)

--- backend/HighFive/analysis_engine/BrokerClient/ResourceUsageCollector/ResourceUsageCollector.cs
+++ backend/HighFive/analysis_engine/BrokerClient/ResourceUsageCollector/ResourceUsageCollector.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+using analysis_engine.BrokerClient.ResourceUsageCollector.Models;
+using analysis_engine.BrokerClient.ResourceUsageCollector.ResourceCollector;
 using Confluent.Kafka;
 
 namespace analysis_engine.BrokerClient.ResourceUsageCollector
@@ -7,29 +12,50 @@ namespace analysis_engine.BrokerClient.ResourceUsageCollector
     {
         public void Run()
         {
-            GetUsage();
-
-            // var config = new ProducerConfig
-            // {
-            //     BootstrapServers = "localhost:9092",
-            //     ClientId = "0",
-            // };
-            //
-            // var clientId = System.Environment.GetEnvironmentVariable("ENGINE_CLIENT_ID");
-            //
-            // TopicPartition partition = new TopicPartition(clientId, 0);
-            //
-            // var producer = new ProducerBuilder<Null, string>(config).Build();
-            //
-            // while (true)
-            // {
-            //     Message<Null, string> msg = new Message<Null, string>();
-            //     producer.Produce(partition, msg);
-            // }
+            while (true)
+            {
+                //Get the external IP of the Analysis Server
+                string externalIpString = new WebClient().DownloadString("http://icanhazip.com")
+                    .Replace("\\r\\n", "").Replace("\\n", "").Trim();
+                var externalIp = IPAddress.Parse(externalIpString);
+            
+                var clientId = Environment.GetEnvironmentVariable("ENGINE_CLIENT_ID");
+                
+                //Create a new ServerInformation object
+                ServerInformation info = new ServerInformation
+                {
+                    ServerIp = externalIp.ToString(),
+                    ServerId = clientId,
+                    ServerPort = "6666",
+                    Credentials = "test1234",
+                    Timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds(),
+                    Usage = GetUsage()
+                };
+                
+                //Create a new Kafka producer
+                var config = new ProducerConfig
+                {
+                    BootstrapServers = "localhost:9092",
+                    ClientId = "0",
+                };
+                TopicPartition partition = new TopicPartition(clientId, 0);
+                var producer = new ProducerBuilder<Null, string>(config).Build();
+            
+                //Send information to Broker
+                Message<Null, string> msg = new Message<Null, string>();
+                msg.Value = info.ToJson();
+                producer.Produce(partition, msg);
+                Thread.Sleep(10000);
+            }
         }
 
-        private string GetUsage()
+        private PerformanceInfo GetUsage()
         {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return new MockResourceCollector().GetPerformance();
+            }
+
             return null;
         }
     }
