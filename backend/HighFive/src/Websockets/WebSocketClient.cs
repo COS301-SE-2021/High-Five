5629c46355cf8e855abd995c258f8528c7f4fa86
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-09-14 11:50:52 +0200

    Backend feature websockets (#269)
    * Fixed some formatting issues.
    
    * UploadTool use cases now return Tool objects.
    
    * StartLiveAnalysis socket request now returns streaming uri.
    
    * Broker connection now sends an initial userId.
    
    * Updated StartLiveAnalysis request.
    
    * Removed old analysis functions.
    
    * Fixed a few syntax errors in socket broker request format.
    
    * Updated socket requests.
    
    * Added asynchronous listening socket function in socket controller.
    
    * Removed blocking broker socket read calls.
    
    * Fixed a few bugs with tool name to id mapper.
    
    * Added IsDefaultTool attribute to OpenAPI Tool Model.
    
    * Added unit tests for User subsystem.
    
    * Added User integration tests.
    
    * Fixed asynchronous socket issues.

--- backend/HighFive/src/Websockets/WebSocketClient.cs
+++ backend/HighFive/src/Websockets/WebSocketClient.cs
@@ -19,7 +19,7 @@ namespace src.Websockets
                 _socket = new ClientWebSocket();
                 await _socket.ConnectAsync(new Uri(uri), CancellationToken.None);
                 await Send(userId);
-                var ack = await Receive();//waits for acknowledgement
+                var ack = Receive().Result;
             }
         }
 
