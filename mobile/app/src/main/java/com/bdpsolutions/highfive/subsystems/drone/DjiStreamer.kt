b23d6501167d4c0e7d76eb42af9afc2fed119e83
Kyle Barry <u19232510@tuks.co.za>
2021-09-16 15:44:11 +0200

    Added updated code for live streaming - Kyle Barry (u19232510)

--- mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DjiStreamer.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DjiStreamer.kt
@@ -4,6 +4,9 @@
 package com.bdpsolutions.highfive.subsystems.drone
 
 import android.util.Log
+import com.bdpsolutions.highfive.constants.Endpoints
+import com.bdpsolutions.highfive.subsystems.drone.model.LiveStreamSocket
+import com.bdpsolutions.highfive.utils.ConcurrencyExecutor
 import com.bdpsolutions.highfive.utils.ToastUtils.setResultToToast
 import dji.sdk.camera.VideoFeeder
 import dji.sdk.camera.VideoFeeder.VideoDataListener
@@ -11,55 +14,73 @@ import dji.sdk.camera.VideoFeeder.VideoFeed
 import dji.sdk.sdkmanager.LiveStreamManager
 import dji.sdk.sdkmanager.DJISDKManager
 import dji.sdk.sdkmanager.LiveStreamManager.OnLiveChangeListener
+import java.net.URI
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
+import com.bdpsolutions.highfive.subsystems.main.HighFiveApplication
 
 
 class DjiStreamer{
-    private val rtmpServerUrl = "rtmp://192.168.11.174"
     private val l = DJISDKManager.getInstance().liveStreamManager
     fun DjiStreamer(){}
     var listener = LiveStreamManager.OnLiveChangeListener {  }
 
-    private fun StartStreaming() {
+    private fun StartStreaming(url: String) {
 
         if (DJISDKManager.getInstance().liveStreamManager.isStreaming) {
             setResultToToast("already started the Stream!")
             return
         }
-        object : Thread() {
-            override fun run() {
-                DJISDKManager.getInstance().liveStreamManager.liveUrl = "rtmp://192.168.11.108:1935/live/test"// + vehicleID);
-                val result = DJISDKManager.getInstance().liveStreamManager.startStream()
-                DJISDKManager.getInstance().liveStreamManager.setStartTime()
-//                setResultToToast(
-//                    """LiveStream Start: $result
-// isVideoStreamSpeedConfigurable:${DJISDKManager.getInstance().liveStreamManager.isVideoStreamSpeedConfigurable}
-// isLiveAudioEnabled:${DJISDKManager.getInstance().liveStreamManager.isLiveAudioEnabled}"""
-//                )
-            }
-        }.start()
+
+        ConcurrencyExecutor.execute {
+            DJISDKManager.getInstance().liveStreamManager.liveUrl = url
+            val result = DJISDKManager.getInstance().liveStreamManager.startStream()
+            DJISDKManager.getInstance().liveStreamManager.setStartTime()
+        }
     }
 
-    public fun setupLiveStream() {
+    fun setupLiveStream() {
         initListener()
         DJISDKManager.getInstance().liveStreamManager.registerListener(listener)
         DJISDKManager.getInstance().liveStreamManager.setAudioStreamingEnabled(false)
         DJISDKManager.getInstance().liveStreamManager.setVideoSource(LiveStreamManager.LiveStreamVideoSource.Primary)
-        StartStreaming()
+        val dialogClickListener =
+            DialogInterface.OnClickListener { _, which ->
+                run {
+                    val requestType = when (which) {
+                        DialogInterface.BUTTON_POSITIVE -> {
+                            "StartLiveAnalysis"
+                        }
+                        else -> "StartLiveStream"
+                    }
+                    ConcurrencyExecutor.execute {
+                        val webSocket = LiveStreamSocket(requestType, URI(Endpoints.WEBSOCKET_URL)) { response ->
+                            StartStreaming(response)
+                        }
+                        webSocket.connect();
+                        while (!webSocket.isClosed) {
+                            Thread.sleep(1000L)
+                        }
+                    }
+                }
+            }
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(HighFiveApplication.getInstance()?.applicationContext!!)
+        builder.setMessage("Live stream or live analysis").setPositiveButton("Live Analysis", dialogClickListener)
+            .setNegativeButton("Live Streaming", dialogClickListener).show()
+
     }
 
     private fun initListener() {
         listener = OnLiveChangeListener {
-                i -> {
-            if(i==0){
-                setResultToToast("Stream started successfully")
-            }else{
-                setResultToToast("Stream initialisation failed")
-            }
-
+                i ->
+            run {
+                if (i == 0) {
+                    setResultToToast("Stream started successfully")
+                } else {
+                    setResultToToast("Stream initialisation failed")
+                }
             }
         }
     }
-
-
-
 }
--- mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneNavigationFragment.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/DroneNavigationFragment.kt
@@ -2,13 +2,8 @@ package com.bdpsolutions.highfive.subsystems.drone
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.bdpsolutions.highfive.databinding.AnalysisFragmentBinding
-import com.bdpsolutions.highfive.subsystems.analysis.viewmodel.AnalysisViewModel
 
 class DroneNavigationFragment: Fragment(){
 
@@ -17,7 +12,7 @@ class DroneNavigationFragment: Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var switchActivityIntent : Intent = Intent(activity, DroneActivity::class.java);
+        val switchActivityIntent = Intent(activity, DroneActivity::class.java);
         startActivity(switchActivityIntent)
         return super.onCreateView(inflater, container, savedInstanceState)
     }
