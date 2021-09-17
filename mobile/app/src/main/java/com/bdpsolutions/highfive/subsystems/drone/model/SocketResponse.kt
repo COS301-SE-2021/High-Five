4ea05290d57653aa5fce36c2a9c781cdaa898529
Kyle Barry <u19232510@tuks.co.za>
2021-09-17 21:40:29 +0200

    Integrated live streaming with Broker. - Kyle Barry (u19232510)

--- mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/model/SocketResponse.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/drone/model/SocketResponse.kt
@@ -3,7 +3,7 @@ package com.bdpsolutions.highfive.subsystems.drone.model
 import com.google.gson.annotations.SerializedName
 
 data class SocketResponse(
-    @SerializedName("publishLink") var publishLink: String?,
+    @SerializedName("playLink") var publishLink: String?,
     @SerializedName("streamId") var streamId: String?,
     @SerializedName("status") var status: String,
     @SerializedName("message") var message: String?,
