8304880a64f99655ba5bb3be14d8a91096424388
Kyle Barry <u19232510@tuks.co.za>
2021-09-17 11:55:10 +0200

    Added ACRA to get crash reports. - Kyle Barry (u19232510)

--- mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/main/HighFiveApplication.kt
+++ mobile/app/src/main/java/com/bdpsolutions/highfive/subsystems/main/HighFiveApplication.kt
@@ -2,9 +2,13 @@ package com.bdpsolutions.highfive.subsystems.main
 
 import android.app.Application
 import android.content.Context
+import com.bdpsolutions.highfive.BuildConfig
 import com.bdpsolutions.highfive.subsystems.drone.DroneApplication
 import com.secneo.sdk.Helper
 import dagger.hilt.android.HiltAndroidApp
+import org.acra.config.mailSender
+import org.acra.data.StringFormat
+import org.acra.ktx.initAcra
 
 @HiltAndroidApp
 class HighFiveApplication : Application(){
@@ -17,6 +21,21 @@ class HighFiveApplication : Application(){
             droneApplication = DroneApplication()
             droneApplication!!.setContext(this)
         }
+
+        initAcra {
+            //core configuration:
+            buildConfigClass = BuildConfig::class.java
+            reportFormat = StringFormat.JSON
+            //each plugin you chose above can be configured in a block like this:
+            mailSender {
+                mailTo = "kylebarry05@gmail.com"
+                subject = "Error"
+                body = "Error"
+                reportAsFile = true
+                reportFileName = "Crash.txt"
+            }
+        }
+
     }
 
     override fun onCreate() {
