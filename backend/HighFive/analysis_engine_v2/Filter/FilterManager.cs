526e586c30e056ce366703af10aa6a18d48529f4
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-18 15:01:50 +0200

    Added comment to Filter manager class

--- backend/HighFive/analysis_engine_v2/Filter/FilterManager.cs
+++ backend/HighFive/analysis_engine_v2/Filter/FilterManager.cs
@@ -1,5 +1,8 @@
 ﻿namespace analysis_engine
 {
+    /**
+     * This abstract class is used as an observer inside filters to manage multiple tool containers running concurrently.
+     */
     public abstract class FilterManager
     {
         public Filter Filter;
