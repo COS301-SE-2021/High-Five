f8a2cad680209b8164c3aaaaf61c39614b7d26ee
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-18 14:57:53 +0200

    Added comment to Filter class

--- backend/HighFive/analysis_engine_v2/Filter/Filter.cs
+++ backend/HighFive/analysis_engine_v2/Filter/Filter.cs
@@ -2,6 +2,12 @@
 
 namespace analysis_engine
 {
+    /**
+     * This class is a container that represents the abstraction of a filter in a pipe filter pattern.
+     * It hold a reference to an input and output pipe aswell as a list of the tool containers running inside the filter.
+     * Additional provision has been made for concurrent tool containers by adding a reference to a concurrent input merger pipe
+     * which acts as a normal pipe except that it will output frames in the correct order even if they are inserted in random order.
+     */
     public class Filter
     {
         public List<ToolContainer> Tools { get; set; }
