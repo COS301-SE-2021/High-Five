e6cae0442c1c095a85af9006a44f2285fc3826c5
ruan-bieldt <82413559+ruan-bieldt@users.noreply.github.com>
2021-09-18 16:05:25 +0200

    Added comment to LinearPipeline class

--- backend/HighFive/analysis_engine_v2/Pipeline/LinearPipeline.cs
+++ backend/HighFive/analysis_engine_v2/Pipeline/LinearPipeline.cs
@@ -1,5 +1,10 @@
 ﻿namespace analysis_engine
 {
+    /**
+     * Description:
+     * ->This is a concrete implementation of the Pipeline abstract class.
+     * A linear pipeline has a single channel of pipes and filters which data moves through to be analyzed.
+     */
     public class LinearPipeline : Pipeline
     {
         public override Filter Init()
