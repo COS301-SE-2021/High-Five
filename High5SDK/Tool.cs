13b6575f73c2d98d4406f3c5067835adfb539a84
Marco <72122625+Marco-Oosthuizen@users.noreply.github.com>
2021-10-11 21:35:35 +0200

    Add files via upload

---
+++ High5SDK/Tool.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace High5SDK
+{
+    public abstract class Tool
+    {
+        public static Buffer Buffer;
+        public abstract Data Process(Data data);
+        public abstract void Init();
+    }
+}
